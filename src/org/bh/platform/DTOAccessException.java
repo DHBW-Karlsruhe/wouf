35a4ad8ca6e68ac755ebfe6a6d6ee96e8140fa9d
Loeckelt.Michael <Loeckelt.Michael@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-03 15:22:55 +0000

    added master DTO and DTOAccessException

---
+++ src/org/bh/platform/DTOAccessException.java
@@ -0,0 +1,27 @@
+package org.bh.platform;
+
+/**
+ * Exception for DTO access
+ * @author Marcus
+ *
+ */
+public class DTOAccessException extends Exception {
+
+	
+	private static final long serialVersionUID = 6655172989787669561L;
+
+	public DTOAccessException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DTOAccessException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DTOAccessException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+}
