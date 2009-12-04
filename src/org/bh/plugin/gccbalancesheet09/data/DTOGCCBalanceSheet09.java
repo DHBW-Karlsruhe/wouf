38d3b432f84b72516d382946829e870078543d94
Loeckelt.Michael <Loeckelt.Michael@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-04 10:23:43 +0000

    renamed hgb to gcc (everything in english, pls)

--- src/org/bh/plugin/gccbalancesheet09/data/DTOGCCBalanceSheet09.java
+++ src/org/bh/plugin/gccbalancesheet09/data/DTOGCCBalanceSheet09.java
@@ -1,4 +1,4 @@
-package org.bh.plugin.hgbbalancesheet09.data;
+package org.bh.plugin.gccbalancesheet09.data;
 
 import java.util.Arrays;
 import org.bh.platform.DTO;
@@ -8,20 +8,19 @@ import org.bh.platform.Value;
  * HGBBalanceSheet DTO 
  *
  * <p>
- * Data Transfer Object to handle HGBBalanceSheets values and methods
+ * Data Transfer Object to handle GCCBalanceSheet09 values and methods
  *
  * @author Michael Lšckelt
- * @version 1.0, 03.12.2009
+ * @version 0.1, 03.12.2009
  *
  */
 
-public class DTOHGBBalanceSheet09 extends DTO<Value> {
+public class DTOGCCBalanceSheet09 extends DTO<Value> {
     
     /**
      * initialize key and method list
      */
-	public DTOHGBBalanceSheet09()
-	{
+	public DTOGCCBalanceSheet09() {
 		// possible keys
 		availableKeys = Arrays.asList(
 				//
