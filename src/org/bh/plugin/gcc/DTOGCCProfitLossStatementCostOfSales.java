ff86cf3a09d5199f0b1be0aacc22db914383b42d
Loeckelt.Michael <Loeckelt.Michael@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-11 23:31:30 +0000

    created dtos for profit/loss statement including all elements and empty methods mister hudson: nobuild

--- src/org/bh/plugin/gcc/DTOGCCProfitLossStatementCostOfSales.java
+++ src/org/bh/plugin/gcc/DTOGCCProfitLossStatementCostOfSales.java
@@ -1,30 +1,188 @@
 package org.bh.plugin.gcc;
 
+import org.bh.calculation.sebi.Calculable;
 import org.bh.data.DTO;
 import org.bh.data.IPeriodicalValuesDTO;
+
+/**
+ * GCCProfitLossStatementCostOfSales DTO.
+ * 
+ * <p>
+ * Data Transfer Object to handle GCCProfitLossStatementCostOfSales values and methods
+ * 
+ * @author Michael Löckelt 
+ * @version 0.1, 11.12.2009
+ * 
+ */
 
 @SuppressWarnings("unchecked")
 public class DTOGCCProfitLossStatementCostOfSales extends DTO implements IPeriodicalValuesDTO {
 	private static final String UNIQUE_ID = "gcc_pls_costofsales";
 	
     public enum Key {
-    	DUMMY,
-    	VALUE1,
-    	VALUE2,
+	
+	/**
+	 * (+) Umsatzerlöse
+	 */
+	UE,
+	
+	/**
+	 * (-) Herstellkosten der zur Erzielung der Umsatzerlöse erbrachten Leistungen
+	 */
+	HK,
+	
+	/**
+	 * (=) Bruttoergebnis vom Umsatz
+	 */
+	@Method
+	BUM,
+	
+	/**
+	 * (-) Vertriebskosten
+	 */
+	VERTK,
+	
+	/**
+	 * (-) Verwaltungskosten
+	 */
+	VERWK,
+	
+	/**
+	 * (+) sonstige betriebliche Erträge 
+	 */
+	SBE,
+	
+	/**
+	 * (-) sonstige betriebliche Aufwendungen
+	 */
+	SBA,
+	
+	/**
+	 * (=) Betriebsergebnis
+	 */
+	@Method
+	BERG,
+	
+	/**
+	 * (+) Erträge aus Beteiligungen
+	 */
+	EBET,
+	
+	/**
+	 * (+) Erträge aus anderen Wertpapieren und Ausleihungen an Finanzanlagevermögens
+	 */
+	EWAF,
+	
+	/**
+	 * (+) sonstige Zinsen und ähnliche Erträge
+	 */
+	SZAE,
+	
+	/**
+	 * (-) Abschreibungen auf Finanzanlagen und übliche Abschreibungen auf Wertpapiere des Umalufvermögens
+	 */
+	AFW,
+	
+	/**
+	 * (-) Zinsen und ähnliche Aufwendungen
+	 */
+	ZA,
+	
+	/**
+	 * (=) Finanzergebnis / Ergebnis der gewöhnlichen Geschäftstätigkeit
+	 */
+	@Method
+	EGG,
+	
+	/**
+	 * (+) außerordentliche Erträge 
+	 */
+	AUE,
+	
+	/**
+	 * (-) außerordentliche Aufwendungen
+	 */
+	AUA,
+	
+	/**
+	 * (=) außerordentliches Ergebnis
+	 */
+	@Method
+	AUERG,
+	
+	/**
+	 * (-) Steuern vom Einkommen und vom Ertrag
+	 */
+	SEE,
+	
+	/**
+	 * (-) sonstige Steuern (insofern erfolgswirksam)
+	 */
+	SS,
+	
+	/**
+	 * (=) Jahresüberschuss / Jahresfehlbetrag
+	 */
+	@Method
+	JUJF;
+	
     }
     
     public DTOGCCProfitLossStatementCostOfSales() {
 		super(Key.values());
 	}
 	
-	@Override
-	public boolean validate() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("This method has not been implemented");
-	}
+    @Override
+    public boolean validate() {
+	// TODO Auto-generated method stub
+	throw new UnsupportedOperationException("This method has not been implemented");
+    }
 
-	@Override
-	public String getUniqueId() {
-		return UNIQUE_ID;
-	}
+    @Override
+    public String getUniqueId() {
+	return UNIQUE_ID;
+    }
+    
+    /**
+     * intermediate result: gross profit on sales
+     * @return Calculable
+     */
+    protected Calculable getBUM() {
+	return null;
+    }
+    
+    /**
+     * intermediate result: operating income
+     * @return Calculable
+     */
+    protected Calculable getBERG() {
+	return null;
+    }
+    
+    /**
+     * intermediate result: result of ordinary activities 
+     * @return Calculable
+     */
+    protected Calculable getEGG() {
+	return null;
+    }
+    
+    /**
+     * intermediate result: extraordinary result
+     * @return Calculable
+     */
+    protected Calculable getAUERG() {
+	return null;
+    }
+    
+    /**
+     * intermediate result: net income
+     * @return Calculable
+     */
+    protected Calculable getJUJF() {
+	return null;
+    }
+    
+ 
+    
 }
