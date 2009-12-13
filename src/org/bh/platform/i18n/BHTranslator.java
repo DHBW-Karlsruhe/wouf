a931650b0a07bb17688e49f4bd688d8107400e1b
Thiele.Klaus <Thiele.Klaus@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-13 11:41:14 +0000

    Translation facilities in 1st working version.

---
+++ src/org/bh/platform/i18n/BHTranslator.java
@@ -0,0 +1,148 @@
+package org.bh.platform.i18n;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Implements the <code>ITranslator</code> Interface for Translation of all i18n
+ * relevant keys in Business Horizon.
+ * 
+ * @author Thiele.Klaus
+ * @version 0.1, 2009/12/12
+ * 
+ */
+public class BHTranslator implements ITranslator {
+
+    /**
+     * Constant for used <code>ResourceBundle</code>.
+     */
+    private static final String BUNDLE = "BHGUIKeys";
+
+    /**
+     * Available <code>Locale</code>s. Only provided if corresponding properties
+     * file is provided.
+     */
+    private static final Locale[] locales = { Locale.US, Locale.GERMANY };
+
+    /**
+     * Currently used <code>Locale</code>.
+     */
+    private Locale locale;
+
+    /**
+     * <code>ResourceBundle</code> for access to the properties file.
+     */
+    private ResourceBundle bundle;
+
+    /**
+     * 
+     */
+    private ArrayList<PropertyChangeListener> listener;
+
+    /**
+     * Default Constructor which instantiates the <code>BHTranslator</code> with
+     * the default locale.
+     */
+    public BHTranslator() {
+	this(Locale.getDefault());
+    }
+
+    /**
+     * Alternative Constructor.
+     * 
+     * @param l
+     *            locale to be used to instantiate the <code>BHTranslator</code>
+     */
+    public BHTranslator(Locale l) {
+	this.locale = l;
+	this.bundle = ResourceBundle
+		.getBundle(BHTranslator.BUNDLE, this.locale);
+	this.listener = new ArrayList<PropertyChangeListener>();
+    }
+
+    /**
+     * Core method to translate keys into domain-specific String to be used in
+     * user facing screen texts.
+     * 
+     * @param key
+     *            key to be translated
+     * @return translated <code>String</code>
+     */
+    public String translate(String key) {
+	return this.bundle.getString(key);
+    }
+
+    /**
+     * Sets a new <code>Locale</code> for further translation.
+     * 
+     * @param locale
+     *            locale to be set
+     */
+    public void setLocale(Locale locale) {
+	Locale l = this.locale;
+	this.locale = locale;
+	this.bundle = ResourceBundle.getBundle(BHTranslator.BUNDLE, locale);
+	this.firePropertyChange("Language", l, this.locale);
+    }
+
+    /**
+     * Returns the currently used <code>Locale</code> for translation.
+     * 
+     * @return the currently used <code>Locale</code>
+     */
+    public Locale getLocale() {
+	return this.locale;
+    }
+
+    /**
+     * Checks if key is available for translation.
+     * 
+     * @param key
+     *            key to be checked
+     * @return true if contains else false.
+     */
+    public boolean containsKey(String key) {
+	return this.bundle.containsKey(key);
+    }
+
+    /**
+     * Provides all available languages.
+     */
+    public Locale[] getAvaiableLocales() {
+	return BHTranslator.locales;
+    }
+
+    /**
+     * add a PropertyChangeListener.
+     */
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+	this.listener.add(l);
+    }
+
+    /**
+     * Remove a PropertyChangeListener.
+     */
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+	this.listener.remove(l);
+    }
+
+    /**
+     * Fire new PropertyChangeEvent to all registered PropertyChangeListeners.
+     * 
+     * @param key
+     *            property that has changed
+     * @param oldValue
+     *            old value
+     * @param newValue
+     *            new value
+     */
+    private void firePropertyChange(String key, Object oldValue, Object newValue) {
+	for (PropertyChangeListener l : this.listener) {
+	    l.propertyChange(new PropertyChangeEvent(this, key, oldValue,
+		    newValue));
+	}
+    }
+}
---
+++ src/org/bh/platform/i18n/ITranslator.java
@@ -0,0 +1,70 @@
+package org.bh.platform.i18n;
+
+import java.beans.PropertyChangeListener;
+import java.util.Locale;
+
+/**
+ * Defines the <code>Interface</code> for a Translation instance that can be
+ * used to translate keys into domain-specific <code>String</code>s.
+ * 
+ * @author Thiele.Klaus
+ * @version 0.1, Dec 6, 2009
+ * 
+ */
+public interface ITranslator {
+
+    /**
+     * @return currently used <code>Locale</code>
+     */
+    Locale getLocale();
+
+    /**
+     * Sets a new locale to be used for further translations.
+     * 
+     * @param locale
+     *            locale to be set
+     */
+    void setLocale(Locale locale);
+
+    /**
+     * Core method to translate key to domain-specific <code>String</code>.
+     * 
+     * @param key
+     *            key to be translated
+     * @return translated <code>String</code>
+     */
+    String translate(String key);
+
+    /**
+     * Checks if key is available for translation.
+     * 
+     * @param key
+     *            key to be checked
+     * @return true if contains else false.
+     */
+    boolean containsKey(String key);
+
+    /**
+     * Returns the available Languages as <code>Locale</code>.
+     * 
+     * @return available languages
+     */
+
+    Locale[] getAvaiableLocales();
+
+    /**
+     * Add a PropertyChangeListener.
+     * 
+     * @param l
+     *            listener to be added
+     */
+    void addPropertyChangeListener(PropertyChangeListener l);
+
+    /**
+     * remove a PropertyChangeListener.
+     * 
+     * @param l
+     *            listener to be removed
+     */
+    void removePropertyChangeListener(PropertyChangeListener l);
+}
