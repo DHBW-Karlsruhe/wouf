b4877ccabba0637e26e63eb58e5f9fbd1a911b7d
Loeckelt.Michael <Loeckelt.Michael@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-15 15:36:50 +0000

    added DTO Project

---
+++ src/org/bh/data/DTOProject.java
@@ -0,0 +1,33 @@
+package org.bh.data;
+
+/**
+ * Project DTO
+ *
+ * <p>
+ * This DTO contains projectdata and acts as a root-element
+ *
+ * @author Michael Löckelt
+ * @version 0.1, 15.12.2009
+ *
+ */
+public class DTOProject extends DTO<DTOScenario> {
+    
+    public enum Keys {
+	/**
+	 * project name
+	 */
+	NAME;
+    }
+    
+    public DTOProject() {
+	super(Keys.values());
+    }
+    
+    @Override
+    public boolean validate() {
+	// TODO Auto-generated method stub
+	throw new UnsupportedOperationException(
+		"This method has not been implemented");
+    }
+
+}
