ed99c16c2f935d673931056153c5fb53d5581603
Vollmer.Robert <Vollmer.Robert@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-15 12:30:34 +0000

    Move direct input to own package

---
+++ plugins-src/directinput/org/bh/plugin/directinput/RandomDirectInputController.java
@@ -0,0 +1,39 @@
+package org.bh.plugin.directinput;
+
+import javax.swing.JPanel;
+
+import org.bh.calculation.sebi.DoubleValue;
+import org.bh.controller.IPeriodGUIController;
+import org.bh.data.DTOPeriod;
+
+
+public class RandomDirectInputController implements IPeriodGUIController {
+	private static final String GUI_KEY = "Random values";
+	private static final int PRIORITY = 0;
+
+	@Override
+	public void editDTO(DTOPeriod period, JPanel panel) {
+		while (period.getChildrenSize() > 0)
+			period.removeChild(0);
+		
+		DTODirectInput dto = new DTODirectInput();
+		dto.put(DTODirectInput.Key.LIABILITIES, new DoubleValue(Math.random() * 200 + 1000));
+		dto.put(DTODirectInput.Key.FCF, new DoubleValue(Math.random() * 20 + 100));
+		period.addChild(dto);
+	}
+
+	@Override
+	public String getGuiKey() {
+		return GUI_KEY;
+	}
+
+	@Override
+	public int getGuiPriority() {
+		return PRIORITY;
+	}
+
+	@Override
+	public void stopEditing() {
+		// not applicable
+	}
+}
