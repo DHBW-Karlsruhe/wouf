e5427586503d537d6c6b0cfddf7d92fa4f328d9e
Tietze.Patrick <Tietze.Patrick@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-16 22:17:15 +0000

    created class BHToolButton

---
+++ src/org/bh/gui/swing/BHToolButton.java
@@ -0,0 +1,57 @@
+package org.bh.gui.swing;
+
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+/**
+ * 
+ * BHToolButton to display buttons on tool bar 
+ *
+ * <p>
+ * This class extends the Swing <code>JButton</code> to display buttons with special icons
+ * on screen.
+ *
+ * @author Tietze.Patrick
+ * @version 0.1, 2009/12/16
+ *
+ */
+
+public class BHToolButton extends JButton{
+    
+    public String toolTip;
+    public String buttonName;
+	
+    
+    public BHToolButton(String imageName,String previous,String toolTipText,String altText){
+	
+	//Look for the image.
+        String imgLocation = "/toolbarButtonGraphics/general/" + imageName + ".gif";
+		//String imgLocation = "images/3D-blue/"+imageName+".png";
+        
+        URL imageURL = BHToolBar.class.getResource(imgLocation);
+        toolTip = toolTipText;
+        buttonName = altText;
+        
+        //Create and initialize the button.
+        setActionCommand(previous);
+        	//setToolTipText(toolTipText);
+        	//this.toolTipText = toolTipText;
+        
+        //addActionListener(this);
+        //addMouseListener(this);
+        
+        	//setIcon(new ImageIcon(imgLocation));
+        	//setPreferredSize(new Dimension(25, 25));
+        
+        if (imageURL != null) {                      
+            //image found
+            setIcon(new ImageIcon(imageURL, altText));
+        } else {                                     
+            //no image found
+            setText(altText);
+            System.err.println("Resource not found: "+ imgLocation);
+        }
+        
+    }
+}
