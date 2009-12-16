f96030cd6e0f393f040ccd1b201e89e86f2572d2
Tietze.Patrick <Tietze.Patrick@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-16 21:31:04 +0000

    edited BHMenuBar and created BHMenuItem class

--- src/org/bh/gui/swing/BHMenuBar.java
+++ src/org/bh/gui/swing/BHMenuBar.java
@@ -3,147 +3,125 @@ package org.bh.gui.swing;
 import java.awt.event.*;
 import javax.swing.*;
 
-/*
- * This class contains all contents of the menu bar
+/**
+ * BHMenuBar to display a menu bar in screen.
  * 
- * @author Patrick Tietze
- * @version 0.1, 03/12/2009
+ * <p>
+ * This class extends the Swing <code>JMenuBar</code> to display a menu bar
+ * on the screen.
+ * 
+ * @author Tietze.Patrick
+ * @version 0.1, 2009/12/16
  * 
  */
 
 public class BHMenuBar extends JMenuBar implements ActionListener{
     
-    /**
-     * Comment for <code>serialVersionUID</code>
-     */
     private static final long serialVersionUID = 1L;
 
     public BHMenuBar(){
-        /*
+        
+    	/**
          * create menu bar
-         */
+         **/
 	
         //create menu --> File
-        JMenu menuDatei = new JMenu("Datei");
-        menuDatei.setMnemonic(KeyEvent.VK_D);
-        add(menuDatei);
+        JMenu menuFile = new JMenu("Datei");
+        //menuFile.setMnemonic(KeyEvent.VK_D);
+        add(menuFile);
         
-        //create menu --> Extras
-        JMenu menuExtras = new JMenu("Extras");
-        menuExtras.setMnemonic(KeyEvent.VK_E);
-        add(menuExtras);
+        //create menu --> Project
+        JMenu menuProject = new JMenu("Project");
+        //menuProject.setMnemonic(KeyEvent.VK_P);
+        add(menuProject);
     
+        //create menu --> Scenario
+        JMenu menuScenario = new JMenu("Szenario");
+        //menuScenario.setMnemonic(KeyEvent.VK_S);
+        add(menuScenario);
+        
+        //create menu --> Bilanz & GuV
+        JMenu menuBilanzGuV = new JMenu("Bilanz & GuV");
+        //menuBilanzGuV.setMnemonic(KeyEvent.VK_B);
+        add(menuBilanzGuV);
+        
+        //create menu --> Options
+        JMenu menuOptions = new JMenu("Optionen");
+        //menuOptions.setMnemonic(KeyEvent.VK_O);
+        add(menuOptions);
+        
         //create menu --> Help
-        JMenu menuHilfe = new JMenu("Hilfe");
-        menuHilfe.setMnemonic(KeyEvent.VK_H);
-        add(menuHilfe);
+        JMenu menuHelp = new JMenu("Hilfe");
+        //menuHelp.setMnemonic(KeyEvent.VK_H);
+        add(menuHelp);
         
-        /*
+        /**
          * create menu items --> file
-         */
-        
-        //file -> new
-        JMenuItem menuDateiNeu = new JMenuItem("Neu");
-        menuDateiNeu.setMnemonic(KeyEvent.VK_N);
-        menuDateiNeu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.ALT_MASK));
-        menuDateiNeu.setActionCommand("neu");
-        menuDateiNeu.addActionListener(this);
-        menuDatei.add(menuDateiNeu);
-        
-        //file -> open
-        JMenuItem menuDateiOpen = new JMenuItem("�ffnen");
-        menuDateiOpen.setMnemonic(KeyEvent.VK_O);
-        menuDateiOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.ALT_MASK));
-        menuDateiOpen.setActionCommand("�ffnen");
-        menuDateiOpen.addActionListener(this);
-        menuDatei.add(menuDateiOpen);
-        
-        //file -> save
-        JMenuItem menuDateiSpeichern = new JMenuItem("Speichern");
-        menuDateiSpeichern.setMnemonic(KeyEvent.VK_S);
-        menuDateiSpeichern.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.ALT_MASK));
-        menuDateiSpeichern.setActionCommand("speichern");
-        menuDateiSpeichern.addActionListener(this);
-        menuDatei.add(menuDateiSpeichern);  
-        
-        //file -> save as
-        JMenuItem menuDateiSpeichernUnter = new JMenuItem("Speichern unter...");
-        menuDateiSpeichernUnter.setActionCommand("speichernUnter");
-        menuDateiSpeichernUnter.addActionListener(this);
-        menuDatei.add(menuDateiSpeichernUnter);   
-        
-        //file -> import
-        JMenuItem menuDateiImport = new JMenuItem("Import");
-        menuDateiImport.setMnemonic(KeyEvent.VK_I);
-        menuDateiImport.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, ActionEvent.ALT_MASK));
-        menuDateiImport.setActionCommand("import");
-        menuDateiImport.addActionListener(this);
-        menuDatei.add(menuDateiImport);  
-        
-        //file -> export
-        JMenuItem menuDateiExport = new JMenuItem("Export");
-        menuDateiExport.setMnemonic(KeyEvent.VK_E);
-        menuDateiExport.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.ALT_MASK));
-        menuDateiExport.setActionCommand("export");
-        menuDateiExport.addActionListener(this);
-        menuDatei.add(menuDateiExport);  
+         **/
                 
-        //file -> exit
-        JMenuItem menuDateiBeenden = new JMenuItem("Beenden");
-        menuDateiBeenden.setMnemonic(KeyEvent.VK_X);
-        menuDateiBeenden.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.ALT_MASK));
-        menuDateiBeenden.setActionCommand("beenden");
-        menuDateiBeenden.addActionListener(this);
-        menuDatei.add(menuDateiBeenden);
-        
-        /*
-         * create menu items --> extras
-         */
-        
-        //extras -> preferences
-        JMenuItem menuExtrasEinstellungen = new JMenuItem("Einstellungen");
-        menuExtrasEinstellungen.setMnemonic(KeyEvent.VK_E);
-        menuExtrasEinstellungen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.ALT_MASK));
-        menuExtrasEinstellungen.setActionCommand("einstellungen");
-        menuExtrasEinstellungen.addActionListener(this);
-        menuExtras.add(menuExtrasEinstellungen);  
-        
-        //extras -> install plugin
-        JMenuItem menuExtrasPlugin = new JMenuItem("Plugin installieren");
-        menuExtrasPlugin.setMnemonic(KeyEvent.VK_P);
-        menuExtrasPlugin.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.ALT_MASK));
-        menuExtrasPlugin.setActionCommand("plugin");
-        menuExtrasPlugin.addActionListener(this);
-        menuExtras.add(menuExtrasPlugin);  
-        
-        /*
-         * create menu items --> help
-         */
-        
-        //help -> HowTo
-        JMenuItem menuHilfeHowTo = new JMenuItem("HowTo");
-        menuHilfeHowTo.setMnemonic(KeyEvent.VK_H);
-        menuHilfeHowTo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.ALT_MASK));
-        menuHilfeHowTo.setActionCommand("howto");
-        menuHilfeHowTo.addActionListener(this);
-        menuHilfe.add(menuHilfeHowTo);  
-        
-        //help -> about
-        JMenuItem menuHilfeUeber = new JMenuItem("�ber...");
-        menuHilfeUeber.setMnemonic(KeyEvent.VK_U);
-        menuHilfeUeber.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, ActionEvent.ALT_MASK));
-        menuHilfeUeber.setActionCommand("�ber");
-        menuHilfeUeber.addActionListener(this);
-        menuHilfe.add(menuHilfeUeber);  
+        menuFile.add(new BHMenuItem("key", 1, 12, "new"));
+        menuFile.add(new BHMenuItem("key", 1, 12, "open"));
+        menuFile.add(new BHMenuItem("key", 1, 12, "close"));
+        menuFile.add(new BHMenuItem("key", 1, 12, "save"));
+        menuFile.add(new BHMenuItem("key", 1, 12, "saveAs"));
+        menuFile.add(new BHMenuItem("key", 1, 12, "close"));
+           
        
-    
+        /**
+         * create menu items --> project
+         **/
+        
+        menuProject.add(new BHMenuItem("key", 1, 12, "create"));
+        menuProject.add(new BHMenuItem("key", 1, 12, "rename"));
+        menuProject.add(new BHMenuItem("key", 1, 12, "duplicate"));
+        menuProject.add(new BHMenuItem("key", 1, 12, "create"));
+        menuProject.add(new BHMenuItem("key", 1, 12, "import"));
+        menuProject.add(new BHMenuItem("key", 1, 12, "export"));
+        menuProject.add(new BHMenuItem("key", 1, 12, "delete"));
+          
+        /**
+         * create menu items --> scenario
+         **/
+        
+        menuScenario.add(new BHMenuItem("key", 1, 12, "create"));
+        menuScenario.add(new BHMenuItem("key", 1, 12, "rename"));
+        menuScenario.add(new BHMenuItem("key", 1, 12, "duplicate"));
+        menuScenario.add(new BHMenuItem("key", 1, 12, "move"));
+        menuScenario.add(new BHMenuItem("key", 1, 12, "delete"));
+      
+        /**
+         * create menu items --> Bilanz & GuV
+         **/
+        
+        menuBilanzGuV.add(new BHMenuItem("key", 1, 12, "show"));
+        menuBilanzGuV.add(new BHMenuItem("key", 1, 12, "create"));
+        menuBilanzGuV.add(new BHMenuItem("key", 1, 12, "import"));
+        menuBilanzGuV.add(new BHMenuItem("key", 1, 12, "edit"));
+        menuBilanzGuV.add(new BHMenuItem("key", 1, 12, "delete"));
+              
+        /**
+         * create menu items --> options
+         **/
+      
+        menuOptions.add(new BHMenuItem("key", 1, 12, "change"));
+        
+        /**
+         * create menu items --> options
+         **/
+      
+        menuHelp.add(new BHMenuItem("key", 1, 12, "userHelp"));
+        menuHelp.add(new BHMenuItem("key", 1, 12, "mathHelp"));
+        menuHelp.add(new BHMenuItem("key", 1, 12, "about"));
+        
+      
     }
     
+
     //ActionListener
     public void actionPerformed(ActionEvent e) {
-	if ("neu".equals(e.getActionCommand())) {
+	if ("new".equals(e.getActionCommand())) {
             
-        } else if ("�ffnen".equals(e.getActionCommand())){
+        } else if ("open".equals(e.getActionCommand())){
         	
         } else {
             
---
+++ src/org/bh/gui/swing/BHMenuItem.java
@@ -0,0 +1,66 @@
+package org.bh.gui.swing;
+
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+/**
+ * BHMenuItem to create and display new menu items in the menu bar.
+ * 
+ * <p>
+ * This class extends the Swing <code>JMenuItem</code> to display menu items
+ * in the menu bar
+ * 
+ * @author Tietze.Patrick
+ * @version 0.1, 2009/12/16
+ * 
+ */
+
+public class BHMenuItem extends JMenuItem implements IBHComponent{
+
+	private String key;
+    private int[] validateRules;
+    
+    /**
+     * create the new menu item
+     * @param key
+     * @param eventKey
+     * @param eventAction
+     * @param actionCommand
+     */
+
+    public BHMenuItem(String key, int eventKey, int eventAction, String actionCommand){
+    	super();
+    	this.key = key;
+    	this.setMnemonic(eventKey);
+    	this.setAccelerator(KeyStroke.getKeyStroke(eventKey, eventAction));
+    	this.setActionCommand(actionCommand);
+    	//this.addActionListener(this);
+    	
+    	
+    }
+	
+	@Override
+	public String getKey() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("This method has not been implemented");
+	}
+
+	@Override
+	public int[] getValidateRules() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("This method has not been implemented");
+	}
+
+	@Override
+	public boolean isTypeValid() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("This method has not been implemented");
+	}
+
+	@Override
+	public void setValidateRules(int[] validateRules) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("This method has not been implemented");
+	}
+
+}
