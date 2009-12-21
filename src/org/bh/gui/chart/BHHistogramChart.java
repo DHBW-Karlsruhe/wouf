731169fb43b17c10ae2bb87a1123200638a2cc26
Zuckschwerdt.Lars <Zuckschwerdt.Lars@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-21 15:17:39 +0000

    added new charttypes and datasetmethods nobuild

--- src/org/bh/gui/chart/BHHistogramChart.java
+++ src/org/bh/gui/chart/BHHistogramChart.java
@@ -1,6 +1,7 @@
 package org.bh.gui.chart;
 
 import java.awt.Component;
+import java.util.List;
 
 import javax.swing.UIManager;
 
@@ -11,6 +12,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.general.Dataset;
 import org.jfree.data.statistics.HistogramDataset;
 
 /**
@@ -23,18 +25,18 @@ import org.jfree.data.statistics.HistogramDataset;
  * @version 0.1, 17.12.2009
  *
  */
-public class BHHistogramChart extends JFreeChart implements IBHComponent{
+public class BHHistogramChart extends JFreeChart implements IBHComponent, IBHAddValue{
 	BHTranslator translator = BHTranslator.getInstance();
     
 	private String key;
 	private JFreeChart chart;
 
 	
-	protected BHHistogramChart(String title, String xAxis, String yAxis, HistogramDataset dataset, String key, Plot plot) {
+	protected BHHistogramChart(String title, String xAxis, String yAxis, Dataset dataset, String key, Plot plot) {
 		super(plot);
 		this.key = key;
 		
-		chart = ChartFactory.createHistogram(title, xAxis, yAxis, dataset, PlotOrientation.VERTICAL, true, true, false);
+		chart = ChartFactory.createHistogram(title, xAxis, yAxis, (HistogramDataset)dataset, PlotOrientation.VERTICAL, true, true, false);
 		chart.getXYPlot().setForegroundAlpha(0.75f);
 		plot.setNoDataMessage(translator.translate("noDataAvailable"));
 		if ("Nimbus".equals(UIManager.getLookAndFeel().getName())) {
@@ -59,7 +61,13 @@ public class BHHistogramChart extends JFreeChart implements IBHComponent{
 	public String getKey() {
 		return key;
 	}
-
+	
+	@Override
+	public void addSeries(Comparable<String> key, double[] values, int bins,
+			double minimum, double maximum) {
+	
+	}
+	
 	/* Specified by interface/super class. */
 	@Override
 	public Component add(Component comp) {
@@ -87,13 +95,31 @@ public class BHHistogramChart extends JFreeChart implements IBHComponent{
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("This method has not been implemented");
 	}
-        @Override
-        public IValue getValue() {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-        @Override
-        public  void setValue(IValue value){
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
+
+	@Override
+	public void addValue(Number value, Comparable<String> columnKey) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("This method has not been implemented");
+	}
+
+	@Override
+	public void addValue(Number value, int rowKey, Comparable<String> columnKey) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("This method has not been implemented");
+	}
+
+	@Override
+	public void addValues(List<?> list, Comparable<String> columnKey) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("This method has not been implemented");
+	}	
+
+    public IValue getValue() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public  void setValue(IValue value){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 	
 }
