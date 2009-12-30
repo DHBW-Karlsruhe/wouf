e55f37bfaeff6f31d1a6a0f46fdf70b3545c6059
Schmalzhaf.Alexander <Schmalzhaf.Alexander@da734bc7-9a39-418a-a4cd-aaf827679e08>
2009-12-30 20:10:26 +0000

    repair-mode

---
+++ src/org/bh/gui/swing/BHScenarioView.java
@@ -0,0 +1,15 @@
+package org.bh.gui.swing;
+
+import javax.swing.JPanel;
+
+import org.bh.gui.View;
+import org.bh.gui.ViewException;
+
+public class BHScenarioView extends View {
+
+	public BHScenarioView(JPanel viewPanel) throws ViewException {
+		super(viewPanel);
+		// TODO Auto-generated constructor stub
+	}
+
+}
