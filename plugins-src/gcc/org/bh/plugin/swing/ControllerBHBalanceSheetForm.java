1ebba2ebae11b13c63f8feb8d5f1c0b50560260a
Hammel.Marco <Hammel.Marco@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-03 14:04:40 +0000

    extend library for crawler

--- plugins-src/gcc/org/bh/plugin/swing/ControllerBHBalanceSheetForm.java
+++ plugins-src/gcc/org/bh/plugin/swing/ControllerBHBalanceSheetForm.java
@@ -5,10 +5,32 @@
 
 package org.bh.plugin.swing;
 
+import java.awt.event.ActionEvent;
+import java.util.Map;
+import org.bh.controller.Controller;
+import org.bh.controller.ControllerException;
+import org.bh.platform.PlatformEvent;
+
 /**
  *
  * @author Marco Hammel
  */
-public class ControllerBHBalanceSheetForm {
+public class ControllerBHBalanceSheetForm extends Controller{
+
+    public void setResult(Map result) throws ControllerException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getGuiKey() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void platformEvent(PlatformEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
