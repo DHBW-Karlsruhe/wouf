06f6c01ba0fd81724acdb39a2d0368254d54512d
Thiele.Klaus <Thiele.Klaus@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-08 16:02:52 +0000

    Enhanced List.

--- src/org/bh/gui/swing/BHSelectionList.java
+++ src/org/bh/gui/swing/BHSelectionList.java
@@ -2,7 +2,10 @@ package org.bh.gui.swing;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JCheckBox;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
@@ -15,23 +18,92 @@ import javax.swing.UIManager;
  * @version 0.5, 2010/01/08
  *
  */
-public class BHSelectionList extends JList {
+public class BHSelectionList extends JList implements MouseListener {
 	
 	/**
 	 * Default Constructor
 	 * @param elements elements to be used.
 	 */
 	public BHSelectionList(Object[] elements) {
-		super(elements);
+		super();
+		for (Object element : elements) {
+			((DefaultListModel) this.getModel()).addElement(new ListElement(element));
+		}
 		this.setCellRenderer(new BHScenarioSelectionRenderer());
 	}
 	
-	public void setSelectedAll() {
-		this.setSelectionInterval(0, this.getModel().getSize()-1);
+	/**
+	 * handle mouse click for selection.
+	 */
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+		// Get index of item clicked
+		int index = this.locationToIndex(e.getPoint());
+		ListElement item = (ListElement) this.getModel().getElementAt(index);
+		 
+		// Toggle selected state
+		item.isSelected =!item.isSelected;
+		if (item.isSelected) {
+			this.addSelectionInterval(index, index);
+		}
+		else {
+			this.removeSelectionInterval(index, index);
+		}
+		// Repaint cell
+		this.repaint(this.getCellBounds(index, index));
 	}
-	
-	public void setSelectedNothing() {
-		this.setSelectedIndex(-1);
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	public class ListElement {
+		
+		/**
+		 * Default for selection of ListElement.
+		 */
+		public static final boolean DEFAULT = true;
+
+		/**
+		 * selection element.
+		 */
+		boolean isSelected;
+		
+		/**
+		 * wrapped value.
+		 */
+		Object value;
+		
+		/**
+		 * Default constructor.
+		 * @param isSelected
+		 * @param value
+		 */
+		public ListElement(Object value) {
+			this.isSelected = ListElement.DEFAULT;
+			this.value = value;
+		}
+		
+		/**
+		 * delegate to wrapped value.
+		 */
+		@Override
+		public String toString() {
+			return value.toString();
+		}
 	}
 
 	/**
@@ -48,15 +120,16 @@ public class BHSelectionList extends JList {
 				int index, boolean isSelected, boolean cellHasFocus) {
 			
 			this.setEnabled(list.isEnabled());
-			this.setSelected(isSelected);
+			
+			this.setSelected(((ListElement) value).isSelected);
 			
 			if (index % 2 == 0) {
 				this.setBackground(UIManager.getColor("control"));
 			} else {
 				this.setBackground(Color.WHITE);
 			}
-			this.setText(value.toString());
 			
+			this.setText(value.toString());
 			return this;
 		}
 	}
