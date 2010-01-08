fe9fe3186790bfbdf267270681a62c14df63e80a
Hammel.Marco <Hammel.Marco@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-08 15:09:41 +0000

    changes in ResultFormulaParser

--- plugins-src/resultAnalysis/org/bh/plugin/resultAnalysis/ResultFormulaParser.java
+++ plugins-src/resultAnalysis/org/bh/plugin/resultAnalysis/ResultFormulaParser.java
@@ -5,7 +5,6 @@
 package org.bh.plugin.resultAnalysis;
 
 import java.util.Map;
-import java.util.logging.Level;
 import net.sourceforge.jeuclid.swing.JMathComponent;
 import org.bh.gui.ViewException;
 import org.bh.platform.formula.FormulaException;
@@ -19,59 +18,59 @@ import org.bh.data.types.Calculable;
  * @author Marco Hammel
  */
 public final class ResultFormulaParser {
-    private static Logger log = Logger.getLogger(ResultFormulaParser.class);
 
+    private static Logger log = Logger.getLogger(ResultFormulaParser.class);
     private static IFormula formula;
     private static IFormulaFactory fFactory = IFormulaFactory.instance;
 
-    JMathComponent getFCFformula() throws ViewException{
+    static JMathComponent getFCFformula() throws ViewException {
         try {
-            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FCF_SHV", getClass().getResourceAsStream("FCF_SHV.xm"));
+            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FCF_SHV", ResultFormulaParser.class.getResourceAsStream("FCF_SHV.xm"));
             return formula.getJMathComponent();
         } catch (FormulaException ex) {
             throw new ViewException("Math ML compile error");
         }
     }
 
-    JMathComponent getFCFformula(Map<String, Calculable> para) throws ViewException{
+    static JMathComponent getFCFformula(Map<String, Calculable> para) throws ViewException {
         try {
-            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FCF_SHV", getClass().getResourceAsStream("FCF_SHV.xm"));
+            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FCF_SHV", ResultFormulaParser.class.getResourceAsStream("FCF_SHV.xm"));
             return formula.getJMathComponentForInputValues(para);
         } catch (FormulaException ex) {
             throw new ViewException("Math ML compile error");
         }
     }
 
-    JMathComponent getAPVformula() throws ViewException{
+    static JMathComponent getAPVformula() throws ViewException {
         try {
-            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("APV_SHV", getClass().getResourceAsStream("FCF_SHV.xm"));
+            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("APV_SHV", ResultFormulaParser.class.getResourceAsStream("FCF_SHV.xm"));
             return formula.getJMathComponent();
         } catch (FormulaException ex) {
             throw new ViewException("Math ML compile error");
         }
     }
 
-     JMathComponent getAPVformula(Map<String, Calculable> para) throws ViewException{
+    static JMathComponent getAPVformula(Map<String, Calculable> para) throws ViewException {
         try {
-            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("APV_SHV", getClass().getResourceAsStream("FCF_SHV.xm"));
+            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("APV_SHV", ResultFormulaParser.class.getResourceAsStream("FCF_SHV.xm"));
             return formula.getJMathComponentForInputValues(para);
         } catch (FormulaException ex) {
             throw new ViewException("Math ML compile error");
         }
     }
 
-     JMathComponent getFTEformula() throws ViewException{
+    static JMathComponent getFTEformula() throws ViewException {
         try {
-            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FTE_SHV", getClass().getResourceAsStream("FCF_SHV.xm"));
+            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FTE_SHV", ResultFormulaParser.class.getResourceAsStream("FCF_SHV.xm"));
             return formula.getJMathComponent();
         } catch (FormulaException ex) {
             throw new ViewException("Math ML compile error");
         }
     }
 
-     JMathComponent getFTEformula(Map<String, Calculable> para) throws ViewException{
+    static JMathComponent getFTEformula(Map<String, Calculable> para) throws ViewException {
         try {
-            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FTE_SHV", getClass().getResourceAsStream("FCF_SHV.xm"));
+            ResultFormulaParser.formula = ResultFormulaParser.fFactory.createFormula("FTE_SHV", ResultFormulaParser.class.getResourceAsStream("FCF_SHV.xm"));
             return formula.getJMathComponentForInputValues(para);
         } catch (FormulaException ex) {
             throw new ViewException("Math ML compile error");
