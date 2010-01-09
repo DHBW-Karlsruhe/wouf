e6c2c04f0c3b939fbe130dd261f302a3fe76fe3a
Vollmer.Robert <Vollmer.Robert@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-09 20:19:45 +0000

    Fix getTax of DTOScenario to accept any Calculable

--- src/org/bh/data/DTOScenario.java
+++ src/org/bh/data/DTOScenario.java
@@ -15,8 +15,8 @@ import org.bh.platform.Services;
  * Scenario DTO
  * 
  * <p>
- * This DTO contains scenariodata, acts as a root-element for periods
- * and as a child for project DTO
+ * This DTO contains scenariodata, acts as a root-element for periods and as a
+ * child for project DTO
  * 
  * @author Michael Löckelt
  * @author Marcus Katzor
@@ -27,65 +27,63 @@ import org.bh.platform.Services;
 public class DTOScenario extends DTO<DTOPeriod> {
 	private static final long serialVersionUID = -2952168332645683232L;
 	private static final Logger log = Logger.getLogger(DTOScenario.class);
-	
+
 	/**
-	 * Used to difference between values in the past and values in the future
-	 * if futureValues is true, new childs are appended at the end of the childlist,
-	 * if not, new childs are appended at the beginning
+	 * Used to difference between values in the past and values in the future if
+	 * futureValues is true, new childs are appended at the end of the
+	 * childlist, if not, new childs are appended at the beginning
 	 */
 	protected boolean futureValues = true;
-	
-	
-	
+
 	protected IShareholderValueCalculator dcfMethod;
-	
+
 	public enum Key {
-		/** 
-		 * equity yield 
+		/**
+		 * equity yield
 		 */
 		REK,
-		
-		/** 
-		 * liability yield 
+
+		/**
+		 * liability yield
 		 */
 		RFK,
-		
+
 		/**
-		 * 	corporate income tax 
+		 * corporate income tax
 		 */
 		CTAX,
-		
+
 		/**
 		 * business tax
 		 */
 		BTAX,
-		
+
 		/**
 		 * summed tax
 		 */
 		@Method
 		TAX,
-		
+
 		/**
 		 * name
 		 */
 		NAME,
-		
+
 		/**
 		 * comment
 		 */
 		COMMENT,
-		
+
 		/**
 		 * Identifier
 		 */
 		IDENTIFIER,
-		
+
 		/**
 		 * DCF method
 		 */
 		DCF_METHOD,
-		
+
 		/**
 		 * Stochastic process
 		 */
@@ -94,22 +92,21 @@ public class DTOScenario extends DTO<DTOPeriod> {
 		@Override
 		public String toString() {
 			return getClass().getName() + "." + super.toString();
-		}	
+		}
 	}
-	
+
 	/**
 	 * Standard constructor
+	 * 
 	 * @author Marcus Katzor
 	 */
-	public DTOScenario()
-	{
+	public DTOScenario() {
 		super(Key.values());
-		log.debug("Object created");
 	}
-	
-    /**
-     * initialize key and method list
-     */
+
+	/**
+	 * initialize key and method list
+	 */
 	public DTOScenario(boolean futureValues) {
 		super(Key.values());
 		this.futureValues = futureValues;
@@ -117,12 +114,12 @@ public class DTOScenario extends DTO<DTOPeriod> {
 
 	@Override
 	public DTOPeriod addChild(DTOPeriod child) throws DTOAccessException {
-		DTOPeriod result = super.addChild(child,this.futureValues);
+		DTOPeriod result = super.addChild(child, this.futureValues);
 		child.scenario = this;
 		refreshPeriodReferences();
 		return result;
 	}
-	
+
 	@Override
 	public DTOPeriod removeChild(int index) throws DTOAccessException {
 		DTOPeriod removedPeriod = super.removeChild(index);
@@ -142,53 +139,59 @@ public class DTOScenario extends DTO<DTOPeriod> {
 		}
 		if (previous != null)
 			previous.next = null;
-		
+
 		log.debug("PeriodReferences refreshed!");
 	}
-	
+
 	/**
 	 * Gets tax for scenario.
+	 * 
 	 * @return Tax for scenario.
 	 */
 	public Calculable getTax() {
-		
-		DoubleValue ctax = (DoubleValue) this.get(DTOScenario.Key.CTAX);
-		DoubleValue btax = (DoubleValue) this.get(DTOScenario.Key.BTAX);
-		
-		DoubleValue myTax = (DoubleValue) btax.mul(new DoubleValue(0.5)).mul(ctax.mul(new DoubleValue(-1)).add(new DoubleValue(1)));
-		myTax = (DoubleValue) myTax.add(ctax);
-		
+		Calculable ctax = (Calculable) getCalculable(DTOScenario.Key.CTAX);
+		Calculable btax = (Calculable) getCalculable(DTOScenario.Key.BTAX);
+
+		Calculable myTax = btax.mul(new DoubleValue(0.5)).mul(
+				ctax.mul(new DoubleValue(-1)).add(new DoubleValue(1)))
+				.add(ctax);
+
 		return myTax;
 	}
 
 	/**
 	 * Returns a reference to the DCF method.
+	 * 
 	 * @return Reference to the DCF method.
 	 */
 	public IShareholderValueCalculator getDCFMethod() {
 		return Services.getDCFMethod(get(Key.DCF_METHOD).toString());
 	}
-	
+
 	/**
 	 * Returns a reference to the stochastic process.
+	 * 
 	 * @return Reference to the stochastic process.
 	 */
 	public IStochasticProcess getStochasticProcess() {
-		return Services.getStochasticProcess(get(Key.STOCHASTIC_PROCESS).toString());
+		return Services.getStochasticProcess(get(Key.STOCHASTIC_PROCESS)
+				.toString());
 	}
-	
+
 	/**
 	 * Returns all keys whose values have to be determined stochastically.
+	 * 
 	 * @return List of keys.
 	 */
 	public List<DTOKeyPair> getPeriodStochasticKeys() {
 		// @TODO
 		throw new UnsupportedOperationException();
 	}
-	
+
 	/**
 	 * This method returns the keys whose values are determined stochastically
 	 * together with their values in the past periods.
+	 * 
 	 * @see #getPeriodStochasticKeys()
 	 * @return Keys and past values.
 	 */
@@ -201,28 +204,29 @@ public class DTOScenario extends DTO<DTOPeriod> {
 			for (DTOPeriod period : children) {
 				if (period == lastPeriod)
 					break;
-				IPeriodicalValuesDTO periodValuesDto = period.getPeriodicalValuesDTO(key.getDtoId());
+				IPeriodicalValuesDTO periodValuesDto = period
+						.getPeriodicalValuesDTO(key.getDtoId());
 				pastValues.add(periodValuesDto.getCalculable(key.getKey()));
 			}
 			map.put(key, pastValues);
 		}
 		return map;
 	}
-	
+
 	/**
-	 * Returns flag for differentiation between
-	 * past and future values.
+	 * Returns flag for differentiation between past and future values.
+	 * 
 	 * @author Marcus Katzor
 	 * @return
 	 */
 	public boolean isFutureValues() {
 		return futureValues;
 	}
-	
+
 	/**
-	 * Sets flag for differentiation between
-	 * past and future values.
-	 * @author Marcus Katzor	
+	 * Sets flag for differentiation between past and future values.
+	 * 
+	 * @author Marcus Katzor
 	 * @param futureValues
 	 */
 	public void setFutureValues(boolean futureValues) {
