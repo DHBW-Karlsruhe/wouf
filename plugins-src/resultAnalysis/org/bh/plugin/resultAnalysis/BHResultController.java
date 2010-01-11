e775a39850f05397e578841915522cff97da5bc9
Hammel.Marco <Hammel.Marco@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-11 13:15:00 +0000

    Output Controller for chart data mapping

---
+++ plugins-src/resultAnalysis/org/bh/plugin/resultAnalysis/BHResultController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bh.plugin.resultAnalysis;
+
+import java.util.Map;
+import org.bh.controller.OutputController;
+import org.bh.data.types.Calculable;
+import org.bh.gui.View;
+
+/**
+ *
+ * @author Marco Hammel
+ */
+public class BHResultController extends OutputController{
+
+    public static enum ChartKeys{
+        BAR_SHAREHOLDER_VALUE,
+        STACK_CV,
+        WATER_CV,
+        BAR_FCF_YEAR,
+        STACK_WACC,
+        BAR_FTE,
+        WATER_FTE,
+    }
+    public BHResultController(View view, Map<String, Calculable> result){
+        super(view);
+    }
+
+    @Override
+    public void setResult(Map<String, Calculable[]> result){
+        for(String s : super.view.getBHchartComponents().keySet()){
+
+        }
+        super.setResult(result);
+    }
+
+}
