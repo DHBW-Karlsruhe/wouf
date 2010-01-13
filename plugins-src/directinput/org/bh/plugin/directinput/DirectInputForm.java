e1cf038c8a65dbd6851572f088c092f99bca297d
Kharitonov.Anton <Kharitonov.Anton@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-13 13:01:58 +0000

    Interval fields were added

--- plugins-src/directinput/org/bh/plugin/directinput/DirectInputForm.java
+++ plugins-src/directinput/org/bh/plugin/directinput/DirectInputForm.java
@@ -3,11 +3,15 @@ package org.bh.plugin.directinput;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
 
+import org.bh.controller.Controller;
 import org.bh.gui.swing.BHDescriptionLabel;
 import org.bh.gui.swing.BHTextField;
+import org.bh.platform.i18n.ITranslator;
 import org.bh.validation.VRIsDouble;
 import org.bh.validation.VRIsPositive;
 import org.bh.validation.VRMandatory;
@@ -39,44 +43,75 @@ public class DirectInputForm extends JPanel {
 	private BHTextField tfminliabilities;
 	private BHTextField tfmaxfcf;
 	private BHTextField tfminfcf;
+	
+	ITranslator translator = Controller.getTranslator();
 
+	
+	public enum Key {
+		/**
+		 * 
+		 */
+		PERIOD_DATA;
+		
+		public String toString() {
+		    return getClass().getName() + "." + super.toString();
+		}
+	}
 	/**
 	 * Constructor.
 	 */
-	public DirectInputForm() {
+	public DirectInputForm(boolean intervalArithmetic) {
+		this.initialize(intervalArithmetic);
+	}
 
+	public void initialize(boolean intervalArithmetic) {
 		String rowDef = "4px,p,4px,p,4px,p,4px";
-		String colDef = "4px,right:pref,4px,150px:grow,4px,pref,24px:grow,pref,4px,max(35px;pref):grow,4px,pref,4px:grow,pref,4px,max(35px;pref):grow,4px,pref,4px:grow";
-
+		String colDef = "4px,right:pref,4px,max(100px;pref):grow,4px,pref,4px";
+		if (intervalArithmetic) {
+			rowDef = "4px,p," + rowDef;
+			colDef += ",max(100px;pref):grow,4px,pref,4px";
+		}
 		FormLayout layout = new FormLayout(colDef, rowDef);
 		this.setLayout(layout);
 
 		CellConstraints cons = new CellConstraints();
-		layout.setColumnGroups(new int[][] { { 4, 10, 16 } });
-
+		
 		this.add(this.getLfcf(), cons.xywh(2, 4, 1, 1));
-		this.add(this.getTffcf(), cons.xywh(4, 4, 1, 1));
-		this.add(this.getLcurrency1(), cons.xywh(6, 4, 1, 1));
 		this.add(this.getLliabilities(), cons.xywh(2, 6, 1, 1));
-		this.add(this.getTfliabilities(), cons.xywh(4, 6, 1, 1));
-		this.add(this.getLcurrency2(), cons.xywh(6, 6, 1, 1));
 
-		// this.add(new JSeparator(SwingConstants.VERTICAL), cons.xywh(8, 2, 1,
-		// 6));
-		// this.add(this.getLmin(), cons.xywh(10, 2, 1, 1));
-		// this.add(new JSeparator(SwingConstants.VERTICAL), cons.xywh(14, 2, 1,
-		// 6));
-		// this.add(this.getLmax(), cons.xywh(16, 2, 1, 1));
-		//
-		// this.add(this.getTfminfcf(), cons.xywh(10, 4, 1, 1));
-		// this.add(this.getLcurrency3(), cons.xywh(12, 4, 1, 1));
-		// this.add(this.getTfmaxfcf(), cons.xywh(16, 4, 1, 1));
-		// this.add(this.getLcurrency4(), cons.xywh(18, 4, 1, 1));
-		//
-		// this.add(this.getTfminliabilities(), cons.xywh(10, 6, 1, 1));
-		// this.add(this.getLcurrency5(), cons.xywh(12, 6, 1, 1));
-		// this.add(this.getTfmaxliabilities(), cons.xywh(16, 6, 1, 1));
-		// this.add(this.getLcurrency6(), cons.xywh(18, 6, 1, 1));
+		if (!intervalArithmetic) {
+			
+			this.add(this.getTffcf(), cons.xywh(4, 4, 1, 1));
+			this.add(this.getLcurrency1(), cons.xywh(6, 4, 1, 1));		
+			this.add(this.getTfliabilities(), cons.xywh(4, 6, 1, 1));
+			this.add(this.getLcurrency2(), cons.xywh(6, 6, 1, 1));
+		} else {
+			layout.setColumnGroups(new int[][] { { 4, 10 } });
+
+			// this.add(new JSeparator(SwingConstants.VERTICAL), cons.xywh(8, 2,
+			// 1,
+			// 6));
+			this.add(this.getLmin(), cons.xywh(4, 2, 1, 1));
+			// this.add(new JSeparator(SwingConstants.VERTICAL), cons.xywh(14,
+			// 2, 1,
+			// 6));
+			this.add(this.getLmax(), cons.xywh(8, 2, 1, 1));
+
+			this.add(this.getLfcf(), cons.xywh(2, 4, 1, 1));
+			this.add(this.getTfminfcf(), cons.xywh(4, 4, 1, 1));
+			this.add(this.getLcurrency3(), cons.xywh(6, 4, 1, 1));
+			this.add(this.getTfmaxfcf(), cons.xywh(8, 4, 1, 1));
+			this.add(this.getLcurrency4(), cons.xywh(10, 4, 1, 1));
+
+			this.add(this.getLliabilities(), cons.xywh(2, 6, 1, 1));
+			this.add(this.getTfminliabilities(), cons.xywh(4, 6, 1, 1));
+			this.add(this.getLcurrency5(), cons.xywh(6, 6, 1, 1));
+			this.add(this.getTfmaxliabilities(), cons.xywh(8, 6, 1, 1));
+			this.add(this.getLcurrency6(), cons.xywh(10, 6, 1, 1));
+		}
+		
+		this.setBorder(BorderFactory
+				.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED),translator.translate(DirectInputForm.Key.PERIOD_DATA)));
 	}
 
 	public BHDescriptionLabel getLmax() {
@@ -218,7 +253,7 @@ public class DirectInputForm extends JPanel {
 	public static void main(String args[]) {
 
 		JFrame test = new JFrame("Test for ViewPeriodData1");
-		test.setContentPane(new DirectInputForm());
+		test.setContentPane(new DirectInputForm(true));
 		test.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
