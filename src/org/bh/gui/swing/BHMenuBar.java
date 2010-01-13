57941e72446f3a652dfe5c4fc2a54abd791485e0
Thiele.Klaus <Thiele.Klaus@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-13 10:13:49 +0000

    Fixes #34 .

--- src/org/bh/gui/swing/BHMenuBar.java
+++ src/org/bh/gui/swing/BHMenuBar.java
@@ -1,5 +1,9 @@
 package org.bh.gui.swing;
 
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
@@ -49,11 +53,11 @@ public class BHMenuBar extends JMenuBar{
 		// menuScenario.setMnemonic(KeyEvent.VK_S);
 		add(menuScenario);
 
-		// create menu --> Bilanz & GuV
-		JMenu menuBilanzGuV = new JMenu(translator
-				.translate("MbalanceProfitLoss"));
-		// menuBilanzGuV.setMnemonic(KeyEvent.VK_B);
-		add(menuBilanzGuV);
+//		// create menu --> Bilanz & GuV
+//		JMenu menuBilanzGuV = new JMenu(translator
+//				.translate("MbalanceProfitLoss"));
+//		// menuBilanzGuV.setMnemonic(KeyEvent.VK_B);
+//		add(menuBilanzGuV);
 
 		// create menu --> Options
 		JMenu menuOptions = new JMenu(translator.translate("Moptions"));
@@ -81,7 +85,7 @@ public class BHMenuBar extends JMenuBar{
 		 * create menu items --> project
 		 **/
 		menuProject.add(new BHMenuItem(PlatformKey.PROJECTCREATE));
-		menuProject.add(new BHMenuItem(PlatformKey.PROJECTRENAME));
+//		menuProject.add(new BHMenuItem(PlatformKey.PROJECTRENAME));
 		menuProject.add(new BHMenuItem(PlatformKey.PROJECTDUPLICATE));
 		menuProject.addSeparator();
 		menuProject.add(new BHMenuItem(PlatformKey.PROJECTIMPORT));
@@ -94,20 +98,20 @@ public class BHMenuBar extends JMenuBar{
 		 * create menu items --> scenario
 		 **/
 		menuScenario.add(new BHMenuItem(PlatformKey.SCENARIOCREATE));
-		menuScenario.add(new BHMenuItem(PlatformKey.SCENARIORENAME));
+//		menuScenario.add(new BHMenuItem(PlatformKey.SCENARIORENAME));
 		menuScenario.add(new BHMenuItem(PlatformKey.SCENARIODUPLICATE));
 		menuScenario.addSeparator();
-		menuScenario.add(new BHMenuItem(PlatformKey.SCENARIOMOVE));
+//		menuScenario.add(new BHMenuItem(PlatformKey.SCENARIOMOVE));
 		menuScenario.add(new BHMenuItem(PlatformKey.SCENARIOREMOVE));
 
 		
-		/**
-		 * create menu items --> Bilanz & GuV
-		 **/
-		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVSHOW, 66));
-		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVCREATE));
-		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVIMPORT));
-		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVREMOVE));
+//		/**
+//		 * create menu items --> Bilanz & GuV
+//		 **/
+//		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVSHOW, 66));
+//		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVCREATE));
+//		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVIMPORT));
+//		menuBilanzGuV.add(new BHMenuItem(PlatformKey.BILANZGUVREMOVE));
 
 		
 		/**
@@ -123,5 +127,21 @@ public class BHMenuBar extends JMenuBar{
 		menuHelp.add(new BHMenuItem(PlatformKey.HELPMATHHELP));
 		menuHelp.addSeparator();
 		menuHelp.add(new BHMenuItem(PlatformKey.HELPINFO, 112));
+	}
+	
+	public class BHMenu extends JMenu {
+		public BHMenu(String title) {
+			super(title);
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			System.err.println("BHMenuBar.paintComponent()");
+			Graphics2D g2d = (Graphics2D) g;
+//			super.paintComponent(g2d);
+//			g2d.rotate(60.0);
+			g2d.setComposite(AlphaComposite.SrcOver.derive(0.8f));
+			super.paintComponent(g2d);
+		}
 	}
 }
