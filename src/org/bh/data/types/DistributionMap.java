6d204c7fad3743b5ec1162b755d84883e506b85d
Weisenburger.Norman <Weisenburger.Norman@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-13 16:37:34 +0000

    [mod] distribution map implements map and iterable interface

--- src/org/bh/data/types/DistributionMap.java
+++ src/org/bh/data/types/DistributionMap.java
@@ -1,145 +1,243 @@
 package org.bh.data.types;
 
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.Map.Entry;
+
 /**
  * 
  * This class provides the functionalities for a mathematical distribution.
  * 
  * @author Sebastian
  * @version 0.2, 06.01.2010
- *
+ * 
+ * @author Norman
+ * @version 0.3, 13.01.2010 implemented interfaces Map, Iterable to harmonize
+ *          interfaces for export (both deterministic and stochastic results
+ *          implement the map interface)
+ * 
  */
-public class DistributionMap{
-	
+public class DistributionMap implements Map<Double, Integer>,
+		Iterable<Entry<Double, Integer>> {
+
 	private double tolerance;
 	private TreeMap<Double, Integer> map;
 	private int amountOfValues;
 	private double sumOfValues;
-	
+
 	/**
 	 * The constructor for the distribution map.
+	 * 
 	 * @param tolerance
-	 * 			Values which are added to the map can be clustered so that they fit into a specific range.
-	 * 			The parameter tolerance describes the width of the cluster.
-	 * 			E.g. tolerance = 5 => values from 0 to 4 are added into the same cluster.
+	 *            Values which are added to the map can be clustered so that
+	 *            they fit into a specific range. The parameter tolerance
+	 *            describes the width of the cluster. E.g. tolerance = 5 =>
+	 *            values from 0 to 4 are added into the same cluster.
 	 */
-	public DistributionMap(double tolerance){
+	public DistributionMap(double tolerance) {
 		this.amountOfValues = 0;
 		sumOfValues = 0;
 		this.tolerance = tolerance;
 		map = new TreeMap<Double, Integer>(new Comparator<Double>() {
 			public int compare(Double o1, Double o2) {
-				if(o1 < o2)
+				if (o1 < o2)
 					return -1;
-				else if(o1 > o2)
+				else if (o1 > o2)
 					return 1;
-				else 
+				else
 					return 0;
 			}
 		});
 	}
+
 	/**
-	 * This method adds the value to the map. 
-	 * If the map contains the value already the occurrence will be increased 
-	 * so that the map describes how often one value has been put into the map.
+	 * This method adds the value to the map. If the map contains the value
+	 * already the occurrence will be increased so that the map describes how
+	 * often one value has been put into the map.
+	 * 
 	 * @param value
-	 * 			The value that should be added to the Map.
+	 *            The value that should be added to the Map.
 	 */
-	public void put(double value){
-		if(tolerance == 0){
-			if(map.containsKey(value)){
+	public void put(double value) {
+		if (tolerance == 0) {
+			if (map.containsKey(value)) {
 				int number = map.get(value);
 				map.put(value, ++number);
-			}else{
+			} else {
 				map.put(value, 1);
 			}
-		}else{
+		} else {
 			double bucket = (Math.round(value / tolerance)) * tolerance;
-			if(map.containsKey(bucket)){
+			if (map.containsKey(bucket)) {
 				int number = map.get(bucket);
 				map.put(bucket, ++number);
-			}else{
+			} else {
 				map.put(bucket, 1);
 			}
 		}
 		amountOfValues++;
 		sumOfValues += value;
 	}
+
 	/**
 	 * This method returns the iterator for the map for displaying purpose.
-	 * @return
-	 * 		the Iterator as an Entry with Key = value and Value = Occurrence of the value.
+	 * 
+	 * @return the Iterator as an Entry with Key = value and Value = Occurrence
+	 *         of the value.
 	 */
-	public Iterator<Entry<Double, Integer>> iterator(){
+	@Override
+	public Iterator<Entry<Double, Integer>> iterator() {
 		return map.entrySet().iterator();
 	}
+
 	/**
-	 * Returns the average of the distribution. Implemented as SUM_OF_ALL_VALUES / OVERALL_NUMBER_OF_VALUES.
-	 * @return
-	 * 		the average.
+	 * Returns the average of the distribution. Implemented as SUM_OF_ALL_VALUES
+	 * / OVERALL_NUMBER_OF_VALUES.
+	 * 
+	 * @return the average.
 	 */
-	public double getAverage(){
+	public double getAverage() {
 		return sumOfValues / amountOfValues;
 	}
+
 	/**
 	 * Returns the standard deviation of the distribution.</br>
-	 * StandardDeviation = &radic;((&sum;(Value² * Occurrence) / OVERALL_NUMBER_OF_VALUES) - AVERAGE²)
-	 * @return
-	 * 		the standard deviation.
+	 * StandardDeviation = &radic;((&sum;(Value² * Occurrence) /
+	 * OVERALL_NUMBER_OF_VALUES) - AVERAGE²)
+	 * 
+	 * @return the standard deviation.
 	 */
-	public double getStandardDeviation(){
+	public double getStandardDeviation() {
 		double sum = 0;
-		for(Entry<Double, Integer> e : map.entrySet()){
+		for (Entry<Double, Integer> e : map.entrySet()) {
 			sum += Math.pow(e.getKey(), 2) * e.getValue();
 		}
 		return Math.sqrt((sum / amountOfValues) - Math.pow(getAverage(), 2));
 	}
+
 	/**
 	 * This method does a value at risk analysis on the distribution.
 	 * 
 	 * @param confidenceCoefficient
-	 * 		This parameter describes the confidence of the user 
-	 * 		with which he wants to know the range of the values.
-	 * @return
-	 * 		The range of values which still fulfill his confidence.</br>
-	 * 		E.g. confidence = 95% => the "real" value will be in between 
-	 * 		the returned borders with a likeliness of 95%
+	 *            This parameter describes the confidence of the user with which
+	 *            he wants to know the range of the values.
+	 * @return The range of values which still fulfill his confidence.</br> E.g.
+	 *         confidence = 95% => the "real" value will be in between the
+	 *         returned borders with a likeliness of 95%
 	 */
-	public IntervalValue valueAtRisk(double confidenceCoefficient){
+	public IntervalValue valueAtRisk(double confidenceCoefficient) {
 		double min = 0;
 		double max = 0;
 		double n = amountOfValues * ((1 - confidenceCoefficient) / 2);
-		
+
 		int sum = 0;
-		for(Entry<Double, Integer> e : map.entrySet()){
+		for (Entry<Double, Integer> e : map.entrySet()) {
 			sum += e.getValue();
-			if(sum >= n){
+			if (sum >= n) {
 				min = e.getKey();
 				break;
 			}
 		}
 		sum = 0;
-		for(Entry<Double, Integer> e : map.descendingMap().entrySet()){
+		for (Entry<Double, Integer> e : map.descendingMap().entrySet()) {
 			sum += e.getValue();
-			if(sum >= n){
+			if (sum >= n) {
 				max = e.getKey();
 				break;
 			}
 		}
 		return new IntervalValue(min, max);
 	}
-//	public double calculateQuality(double standardDeviation, double mean) throws MathException{
-//	System.out.println(standardDeviation);
-//	double qualityIndicator = 0;
-//	NormalDistributionImpl ndi = new NormalDistributionImpl(mean, standardDeviation);
-//	//NormalDistributionImpl ndi = new NormalDistributionImpl(mean, 30);
-//	//ndi.cumulativeProbability(0.01);
-//	for(Entry<Double, Integer> e : map.entrySet()){
-//		qualityIndicator += Math.pow(ndi.cumulativeProbability(e.getKey()) - (e.getValue() / amountOfValues), 2);
-//	}
-//	return qualityIndicator / amountOfValues;
-//}
+
+	// public double calculateQuality(double standardDeviation, double mean)
+	// throws MathException{
+	// System.out.println(standardDeviation);
+	// double qualityIndicator = 0;
+	// NormalDistributionImpl ndi = new NormalDistributionImpl(mean,
+	// standardDeviation);
+	// //NormalDistributionImpl ndi = new NormalDistributionImpl(mean, 30);
+	// //ndi.cumulativeProbability(0.01);
+	// for(Entry<Double, Integer> e : map.entrySet()){
+	// qualityIndicator += Math.pow(ndi.cumulativeProbability(e.getKey()) -
+	// (e.getValue() / amountOfValues), 2);
+	// }
+	// return qualityIndicator / amountOfValues;
+	// }
+	@Override
+	public void clear() {
+		map.clear();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return map.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return map.containsValue(value);
+	}
+
+	@Override
+	public Set<Entry<Double, Integer>> entrySet() {
+		return map.entrySet();
+	}
+
+	@Override
+	public Integer get(Object key) {
+		return map.get(key);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	@Override
+	public Set<Double> keySet() {
+		return map.keySet();
+	}
+
+	/**
+	 * Has the same functionality as put(double) value'll be ignored. Return
+	 * value is 1 every time because return value of put(double) is void
+	 * 
+	 */
+	@Override
+	public Integer put(Double key, Integer value) {
+		put(key);
+		return 1;
+	}
+
+	/**
+	 * Has the same functionality as multiple invocations of put(double)
+	 * value'll be ignored. Return value is 1 every time because return value of
+	 * put(double) is void
+	 * 
+	 */
+	@Override
+	public void putAll(Map<? extends Double, ? extends Integer> m) {
+		for (double d : m.keySet()) {
+			put(d);
+		}
+	}
+
+	@Override
+	public Integer remove(Object key) {
+		return map.remove(key);
+	}
+
+	@Override
+	public int size() {
+		return map.size();
+	}
+
+	@Override
+	public Collection<Integer> values() {
+		return map.values();
+	}
 }
