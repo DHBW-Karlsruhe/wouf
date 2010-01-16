fd1850ebf7a48c4e60df06459feb9954b5d42c32
Weisenburger.Norman <Weisenburger.Norman@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-16 19:04:51 +0000

    modified BHCheckBox ti implement IBHmodelComponent

--- src/org/bh/gui/swing/BHCheckBox.java
+++ src/org/bh/gui/swing/BHCheckBox.java
@@ -1,14 +1,21 @@
 package org.bh.gui.swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.JCheckBox;
 
 import org.apache.log4j.Logger;
 import org.bh.data.types.IValue;
+import org.bh.data.types.IntegerValue;
 import org.bh.data.types.StringValue;
 import org.bh.gui.CompValueChangeManager;
-import org.bh.gui.swing.BHTextField;
-import org.bh.gui.swing.IBHModelComponent;
 import org.bh.platform.PlatformEvent;
 import org.bh.platform.Services;
+import org.bh.platform.PlatformEvent.Type;
+import org.bh.platform.i18n.ITranslator;
 import org.bh.validation.ValidationRule;
 
 /**
@@ -19,35 +26,23 @@ import org.bh.validation.ValidationRule;
  * @author Kharitonov.Anton
  * @version 0.1, 2010/01/07
  * 
+ * @author Norman
+ * @version 0.2, 16.01.2010
+ * 
  */
 
 // TODO Hints setzen!!! Noch werden für Textfields keine Hints erzeugt
-public class BHCheckBox extends JCheckBox implements IBHComponent {
+public class BHCheckBox extends JCheckBox implements IBHModelComponent {
+	
+	private static final Logger log = Logger.getLogger(BHCheckBox.class);
 	/**
 	 * unique key to identify Label.
 	 */
-	Object key;
+	String key;
+	String hint;
 
-	private ValidationRule[] validationRules = new ValidationRule[0];
-	private String inputHint;
-	private boolean changeListenerEnabled = true;
-	private final CompValueChangeManager valueChangeManager = new CompValueChangeManager();
-	private static final Logger log = Logger.getLogger(BHTextField.class);
-
-
-	// TODO Konsoliedieren der Konstruktoren (nicht alle notwendig)
-
-
-	/**
-	 * Constructor to create new <code>BHCheckBox</code>. with key based text
-	 * 
-	 * @param key
-	 *            unique key
-	 */
-	public BHCheckBox(String key) {
-	    super();
-	    this.key = key;
-	}
+	boolean changeListenerEnabled = true;
+	final CompValueChangeManager valueChangeManager = new CompValueChangeManager();
 
 	/**
 	 * Constructor to create new <code>BHCheckBox</code>. with key based text
@@ -57,46 +52,102 @@ public class BHCheckBox extends JCheckBox implements IBHComponent {
 	 */
 	public BHCheckBox(Object key) {
 		super();
-		this.key = key;
+		this.key = key.toString();
+		reloadText();
+		addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (changeListenerEnabled)
+					valueChangeManager
+							.fireCompValueChangeEvent(BHCheckBox.this);
+			}
+		});
+		setSelected(false);
 	}
 
 	@Override
 	public String getKey() {
-		return key.toString();
-	}
-
-	public void setInputHint(String inputHint) {
-		this.inputHint = inputHint;
-	}
-
-	// TODO ins Interface aufnehmen?
-	public String getInputHint() {
-		return this.inputHint;
-	}
-
-//	/**
-//	 * set properties of instance.
-//	 */
-//	private void setProperties() {
-//		Services.addPlatformListener(this);
-//	}
-
-	public String getBHHint() {
-
-	    throw new UnsupportedOperationException("This method has not been implemented");
-	}
-
-	public void reloadText() {
-	}
-
-	public void platformEvent(PlatformEvent e) {
-	    // TODO Auto-generated method stub
-	    throw new UnsupportedOperationException("This method has not been implemented");
+		return key;
 	}
 
 	@Override
 	public String getHint() {
-	    // TODO Auto-generated method stub
-	    throw new UnsupportedOperationException("This method has not been implemented");
+		return hint;
+	}
+
+	// /**
+	// * set properties of instance.
+	// */
+	// private void setProperties() {
+	// Services.addPlatformListener(this);
+	// }
+
+	public String getBHHint() {
+
+		throw new UnsupportedOperationException(
+				"This method has not been implemented");
+	}
+
+	public void platformEvent(PlatformEvent e) {
+		if (e.getEventType() == Type.LOCALE_CHANGED) {
+			reloadText();
+		}
+	}
+
+	@Override
+	public ValidationRule[] getValidationRules() {
+		return new ValidationRule[0];
+	}
+
+	@Override
+	public IValue getValue() {
+		if(isSelected()) {
+			return new IntegerValue(1);
+		}
+		return new IntegerValue(0);
+	}
+
+	@Override
+	public CompValueChangeManager getValueChangeManager() {
+		return valueChangeManager;
+	}
+
+	@Override
+	public void setValidationRules(ValidationRule[] validationRules) {
+		// noop
+	}
+
+	@Override
+	public void setValue(IValue value) {
+		if (value == null) {
+			return;
+		}
+		if(value instanceof IntegerValue) {
+			int valInt = ((IntegerValue) value).getValue();
+			if(valInt >= 1) {
+				setSelected(true);
+			}
+			if(valInt == 0) {
+				setSelected(false);
+			}
+		}
+		if (value instanceof StringValue) {
+			String valStr = ((StringValue) value).getString();
+			if (valStr.toLowerCase().equals("true")) {
+				setSelected(true);
+				return;
+			}
+			if (valStr.toLowerCase().equals("false")) {
+				setSelected(false);
+				return;
+			}
+			return;
+		}
+	}
+	
+	protected void reloadText() {
+		hint = Services.getTranslator().translate(key, ITranslator.LONG);
+		setToolTipText(hint);
+		updateUI();
 	}
 }
