b45f5e1d6c1d63dc3fb1c9c1206a907e76694ffd
Kharitonov.Anton <Kharitonov.Anton@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-20 12:02:13 +0000

    two buttons were added to GCCCombinedForm

--- plugins-src/gcc/org/bh/plugin/gcc/swing/GCCCombinedForm.java
+++ plugins-src/gcc/org/bh/plugin/gcc/swing/GCCCombinedForm.java
@@ -2,13 +2,27 @@ package org.bh.plugin.gcc.swing;
 
 import javax.swing.JPanel;
 
+import org.bh.gui.swing.BHButton;
+
 import com.jgoodies.forms.layout.FormLayout;
 
 @SuppressWarnings("serial")
 public class GCCCombinedForm extends JPanel {
+	private JPanel pbuttons;
 	public GCCCombinedForm(JPanel balanceSheetForm, JPanel plsForm) {
-		setLayout(new FormLayout("4px,p,4px", "p,p"));
-		add(balanceSheetForm, "2,1,1,1,left,default");
+		setLayout(new FormLayout("4px,p,4px,p,4px", "p,p"));
+		add(balanceSheetForm, "2,1,2,1,left,default");
 		add(plsForm, "2,2,1,1,left,default");
+		add(this.getPbuttos(),"4,2,1,1,right,default");
+	}
+	
+	public JPanel getPbuttos(){
+		if (pbuttons == null){
+			pbuttons = new JPanel();
+			pbuttons.setLayout(new FormLayout("4px,p,4px,p,4px", "p"));
+			pbuttons.add(new BHButton("Bbsexport"),"2,1,1,1,right,bottom");
+			pbuttons.add(new BHButton("Bbsimport"),"4,1,1,1,right,bottom");
+		}
+		return pbuttons;
 	}
 }
