3b13da155cccf75a7a67e61ad42ec2b0766815a7
Kharitonov.Anton <Kharitonov.Anton@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-20 20:56:27 +0000

    TextArea is hidden

--- src/org/bh/gui/chart/BHChartPanel.java
+++ src/org/bh/gui/chart/BHChartPanel.java
@@ -5,18 +5,24 @@
 
 package org.bh.gui.chart;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
 
 import org.bh.gui.swing.IBHComponent;
 import org.bh.platform.Services;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.validation.view.ValidationResultViewFactory;
 
 /**
  *
@@ -35,18 +41,46 @@ public class BHChartPanel extends JPanel implements IBHComponent, IBHAddValue, I
     	this.key = key.toString();
         this.chartClass = chartClass;
         this.chartInstance = chartInstance;
-    	this.setLayout(new BorderLayout());
-        this.add(new ChartPanel(chart), BorderLayout.CENTER);
-        JTextArea description = new JTextArea(Services.getTranslator().translate(key + BHChart.DESC));
+    	this.setLayout(new FormLayout("pref,pref","p"));
+        this.add(new ChartPanel(chart), "1,1");
+        JTextArea description = new JTextArea(1,20);
+        description.setText(Services.getTranslator().translate(key + BHChart.DESC));
         description.setEditable(false);
+        description.setVisible(false);
         description.setAutoscrolls(true);
         description.setBorder(BorderFactory.createLoweredBevelBorder());
         description.setLineWrap(true);
         description.setWrapStyleWord(true);
-        description.setPreferredSize(new Dimension(250, 50));
-    	this.add(description, BorderLayout.EAST);
+//        description.setPreferredSize(new Dimension(250, description.getPreferredSize().height));
+        
+//    	this.add(description, BorderLayout.EAST);
+    	JPanel panel = new JPanel(new FormLayout("pref","p,p"));
+    	
+    	JToggleButton info = new JToggleButton();
+    	info.setIcon(ValidationResultViewFactory.getInfoIcon());
+    	info.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JToggleButton button = (JToggleButton)e.getSource();
+				JTextArea ta = new JTextArea();
+				for (Component comp : button.getParent().getComponents()){
+					if(comp instanceof JTextArea)
+						ta = (JTextArea)comp;
+				}
+				if (button.isSelected()){
+					ta.setVisible(true);
+				}
+				else{
+					ta.setVisible(false);
+				}
+			}
+		});
+    	panel.add(info,"1,1,left,top");
+    	panel.add(description, "1,2");
+    	
+    	this.add(panel, "2,1,left,top");
     	this.chart = chart;
     }
+ 
 
     /**
      * get the semantic object for the chart by changing dataset at runtime
