193eb3b50636c38e70327d30cc9e57d94c849fa6
Zuckschwerdt.Lars <Zuckschwerdt.Lars@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-22 12:26:01 +0000

    code review and javadoc nobuild

--- src/org/bh/gui/swing/BHPopupMenu.java
+++ src/org/bh/gui/swing/BHPopupMenu.java
@@ -2,21 +2,22 @@ package org.bh.gui.swing;
 
 import javax.swing.JPopupMenu;
 import javax.swing.border.BevelBorder;
-
+/**
+ * 
+ * Class to create the Contextmenu
+ *
+ * <p>
+ * new PopupMenu for Contextmenu is created
+ *
+ * @author Lars.Zuckschwerdt
+ * @version 1.0, 22.01.2010
+ *
+ */
 public class BHPopupMenu extends JPopupMenu{
 	JPopupMenu popup;
 	public BHPopupMenu(){
 		popup = new JPopupMenu();
-	
-		//TODO Lars - wird der gebraucht?
-//		ActionListener menuListener = new ActionListener() {
-//	        public void actionPerformed(ActionEvent event) {
-//	            System.out.println("Popup menu item ["
-//	                + event.getActionCommand() + "] was pressed.");
-//	          };
-//	    };
-	   
-	    popup.setLabel("Justification");
+		popup.setLabel("Justification");
 	    popup.setBorder(new BevelBorder(BevelBorder.RAISED));	    
     };
 }
