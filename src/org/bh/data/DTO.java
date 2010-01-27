0f6daa406351b29e0684dc5edad0e71847211b00
Weisenburger.Norman <Weisenburger.Norman@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-27 15:01:22 +0000

    fixes export keys

--- src/org/bh/data/DTO.java
+++ src/org/bh/data/DTO.java
@@ -270,10 +270,8 @@ public abstract class DTO<ChildT extends IDTO> implements IDTO<ChildT> {
 		if (!children.contains(child)) {
 			if (addLast) {
 				children.addLast(child);
-				System.out.println("---last");
 			} else {
 				children.addFirst(child);
-				System.out.println("---first");
 			}
 			if (throwEvents)
 				Services.firePlatformEvent(new PlatformEvent(this,
