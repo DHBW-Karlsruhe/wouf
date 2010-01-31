1e795154fa36b523ff215b6f8b7b7dbe13f46527
Weisenburger.Norman <Weisenburger.Norman@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-01-31 18:23:48 +0000

    fixes export for stochastic results (incl alignment of buttons)

--- plugins-src/stochasticResultAnalysis/org/bh/plugin/stochasticResultAnalysis/BHStochasticResultPanel.java
+++ plugins-src/stochasticResultAnalysis/org/bh/plugin/stochasticResultAnalysis/BHStochasticResultPanel.java
@@ -1,25 +1,17 @@
 package org.bh.plugin.stochasticResultAnalysis;
 
+import info.clearthought.layout.TableLayout;
+import info.clearthought.layout.TableLayoutConstants;
+
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
 import org.apache.log4j.Logger;
 import org.bh.data.DTOScenario;
 import org.bh.data.types.DistributionMap;
-import org.bh.gui.chart.BHChartPanel;
 import org.bh.gui.swing.comp.BHButton;
-import org.bh.gui.swing.importexport.BHDataExchangeDialog;
-import org.bh.platform.IImportExport;
-import org.bh.platform.IPrint;
-import org.bh.platform.Services;
-import org.jfree.chart.JFreeChart;
 
 @SuppressWarnings("serial")
 public class BHStochasticResultPanel extends JPanel{
@@ -39,59 +31,71 @@ public class BHStochasticResultPanel extends JPanel{
 	private void initialize(){
 		this.setLayout(new BorderLayout());
 		exportButton = new BHButton(BHStochasticResultController.PanelKeys.EXPORTSCENARIO);
-		exportButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-				BHDataExchangeDialog dialog = new BHDataExchangeDialog(null,
-						true);
-				dialog.setAction(IImportExport.EXP_SCENARIO_RES);
-				dialog.setModel(scenario);
-				dialog.setResults(result);
-				
-				dialog.setIconImages(Services.setIcon());
-								
-				List<JFreeChart> charts = new ArrayList<JFreeChart>();
-				for(Component c : mainPanel.getComponents()) {
-					if(c instanceof BHChartPanel) {
-						BHChartPanel cp = (BHChartPanel) c;
-						charts.add(cp.getChart());
-					}
-				}
-				dialog.setCharts(charts);
-				
-				dialog.setVisible(true);
-
-			}
-		});
+//		exportButton.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//
+//				BHDataExchangeDialog dialog = new BHDataExchangeDialog(null,
+//						true);
+//				dialog.setAction(IImportExport.EXP_SCENARIO_RES);
+//				dialog.setModel(scenario);
+//				dialog.setResults(result);
+//				
+//				dialog.setIconImages(Services.setIcon());
+//								
+//				List<JFreeChart> charts = new ArrayList<JFreeChart>();
+//				for(Component c : mainPanel.getComponents()) {
+//					if(c instanceof BHChartPanel) {
+//						BHChartPanel cp = (BHChartPanel) c;
+//						charts.add(cp.getChart());
+//					}
+//				}
+//				dialog.setCharts(charts);
+//				
+//				dialog.setVisible(true);
+//
+//			}
+//		});
 		
 		// printButton
 		printButton = new BHButton(BHStochasticResultController.PanelKeys.PRINTSCENARIO);
-		printButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Map<String, IPrint> pPlugs = Services.getPrintPlugins(IPrint.PRINT_SCENARIO_RES);
-				
-				List<JFreeChart> charts = new ArrayList<JFreeChart>();
-				for(Component c : mainPanel.getComponents()) {
-					if(c instanceof BHChartPanel) {
-						BHChartPanel cp = (BHChartPanel) c;
-						charts.add(cp.getChart());
-					}
-				}
-				((IPrint) pPlugs.values().toArray()[0]).printScenarioResults(scenario, result, charts);
-			}
-		});
+//		printButton.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				Map<String, IPrint> pPlugs = Services.getPrintPlugins(IPrint.PRINT_SCENARIO_RES);
+//				
+//				List<JFreeChart> charts = new ArrayList<JFreeChart>();
+//				for(Component c : mainPanel.getComponents()) {
+//					if(c instanceof BHChartPanel) {
+//						BHChartPanel cp = (BHChartPanel) c;
+//						charts.add(cp.getChart());
+//					}
+//				}
+//				((IPrint) pPlugs.values().toArray()[0]).printScenarioResults(scenario, result, charts);
+//			}
+//		});
 		
 		
 		mainPanel = new StochasticPanel();
 		this.add(mainPanel, BorderLayout.CENTER);
-		JPanel buttons = new JPanel();
 		
-		buttons.add(exportButton, BorderLayout.WEST);
-		buttons.add(printButton, BorderLayout.EAST);
-		add(buttons, BorderLayout.NORTH);
+		double border = 10;
+		double size2[][] = {
+			{ border, TableLayoutConstants.PREFERRED, border, TableLayoutConstants.PREFERRED, border }, // Columns
+			{ border, TableLayoutConstants.PREFERRED, border} };
+		JPanel exportArea = new JPanel(new TableLayout(size2));
+		exportArea.add(exportButton, "1,1" );
+		
+		exportArea.add(printButton, "3,1");
+		exportArea.setMaximumSize(new Dimension(200, 40));
+		
+		
+//		JPanel buttons = new JPanel();
+//		
+//		buttons.add(exportButton, BorderLayout.WEST);
+//		buttons.add(printButton, BorderLayout.EAST);
+		add(exportArea, BorderLayout.NORTH);
 	}
 }
