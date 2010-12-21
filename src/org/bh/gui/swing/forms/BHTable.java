9456eacd32acd5cc3f3c08f287626003d73fbeb9
Maisel.Petrick <Maisel.Petrick@da734bc7-9a39-418a-a4cd-aaf827679e08>
2010-12-21 09:48:19 +0000

    neue Klasse für Tables erstellt. Diese sind übersetzbar (#215)

---
+++ src/org/bh/gui/swing/forms/BHTable.java
@@ -0,0 +1,61 @@
+package org.bh.gui.swing.forms;
+
+import javax.swing.JTable;
+
+import org.bh.data.types.IValue;
+import org.bh.platform.IPlatformListener;
+import org.bh.platform.PlatformEvent;
+import org.bh.platform.PlatformEvent.Type;
+import org.bh.platform.Services;
+import org.bh.platform.i18n.ITranslator;
+
+/**
+ * 
+ * <p>
+ * This class extends the Swing <code>JTable</code> so it's translatable.
+ * 
+ * @author Maisel.Patrick
+ * @version 0.1, 2010/12/20
+ * 
+ */
+
+@SuppressWarnings("serial")
+public class BHTable extends JTable implements IPlatformListener {
+
+	static final ITranslator translator = Services.getTranslator();
+
+	/**
+	 * unique key [] to identify column names
+	 */
+	String[] key;
+	int length;
+	Object[][] data;
+
+	public BHTable(Object[][] data, final String[] key) {
+		super(data, key);
+		for (int i = 0; i <= this.length - 1; i++) {
+			key[i] = translator.translate(key[i]);
+		}
+		this.data = data;
+		this.key = key;
+		this.length = key.length;
+		Services.addPlatformListener(this);
+		this.reloadText();
+	}
+
+	@Override
+	public void platformEvent(PlatformEvent e) {
+		if (e.getEventType() == Type.LOCALE_CHANGED) {
+			reloadText();
+		}
+	}
+
+	protected void reloadText() {
+		// TODO muss noch ausprogrammiert werden
+		for (int i = 0; i <= this.length - 1; i++) {
+			this.getColumnModel().getColumn(i)
+					.setHeaderValue(translator.translate(this.key[i]));
+		}
+
+	}
+}
