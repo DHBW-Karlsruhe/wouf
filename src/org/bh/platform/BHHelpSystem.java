6f23b78839d5fb943867dde1171248a449841bdb
Maisel.Petrick <Maisel.Petrick@da734bc7-9a39-418a-a4cd-aaf827679e08>
2011-01-05 12:35:12 +0000

    Änderungen für multilinguale Hilfetexte. Für jede Sprache muss nun eine eigene .jar Datei erstellt werden. Dabei MUSS das Sprachkürzel (sowie Locale) IMMER 2 Buchstaben haben, da sonst die URL nicht richtig gefunden wird!

--- src/org/bh/platform/BHHelpSystem.java
+++ src/org/bh/platform/BHHelpSystem.java
@@ -2,46 +2,59 @@ package org.bh.platform;
 
 import java.awt.Dimension;
 import java.net.URL;
+import java.util.Locale;
 
 import javax.help.HelpSet;
 import javax.help.JHelp;
 import javax.swing.JPanel;
+
 /**
  * 
  * method to create the helpSystem
- *
+ * 
  * <p>
  * the Helpsystem is built.
- *
+ * 
  * @author Lars.Zuckschwerdt
  * @version 1.0, 31.12.2009
- *
+ * 
  */
 @SuppressWarnings("serial")
-public class BHHelpSystem extends JPanel{
+public class BHHelpSystem extends JPanel {
 	JHelp helpViewer = null;
-	
-	protected BHHelpSystem(String ID){
+
+	protected BHHelpSystem(String ID) {
 		this.initialise(ID);
 	}
+
 	/**
 	 * method to initialise the HelpFrame
+	 * 
 	 * @param ID
-	 * 	ID to get the choosen Help. 
-	 * 	mathe for MathHelp
-	 * 	prog for Usermanual
+	 *            ID to get the choosen Help. mathe for MathHelp user for
+	 *            Usermanual
 	 */
-	public void initialise(String ID){
-		
-			try {
-		      	ClassLoader cl = BHHelpSystem.class.getClassLoader();
-		      	URL url = HelpSet.findHelpSet(cl, "jhelpset.hs");
-		      	helpViewer = new JHelp(new HelpSet(cl, url));
-		      	helpViewer.setPreferredSize(new Dimension(930,650));
-		      	helpViewer.setCurrentID(ID);
-		      	} catch (Exception e) {
-				System.err.println("API Help Set not found");
-			}
+	public void initialise(String ID) {
+
+		try {
+			ClassLoader cl = BHHelpSystem.class.getClassLoader();
+			Locale l = Services.getTranslator().getLocale();
+			URL url = HelpSet.findHelpSet(cl, "jhelpset.hs");
+			
+			// URL wird hier geändert, damit die richtige Hilfedatei anhand der Locale gewählt wird
+			int urlsize = url.toString().length();
+			String urlold = url.toString().substring(0, urlsize - 19);
+			String urlnew = (urlold + l.toString() + ".jar!/jhelpset.hs");
+			URL urlnewurl = new URL(urlnew);
+			// URL verändert
+			
+			helpViewer = new JHelp(new HelpSet(cl, urlnewurl));
+			helpViewer.setPreferredSize(new Dimension(930, 650));
+			helpViewer.setCurrentID(ID);
+
+		} catch (Exception e) {
+			System.err.println("API Help Set not found");
+		}
 		this.add(helpViewer);
 		this.setVisible(true);
 
