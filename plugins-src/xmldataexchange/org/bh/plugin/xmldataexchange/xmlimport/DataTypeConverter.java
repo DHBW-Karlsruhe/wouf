aed213dd7a96d04dfaa8c1429b1f044301aff490
Masiello.Vito <Masiello.Vito@da734bc7-9a39-418a-a4cd-aaf827679e08>
2011-01-19 08:20:13 +0000

    Änderungen für XOM Library

--- plugins-src/xmldataexchange/org/bh/plugin/xmldataexchange/xmlimport/DataTypeConverter.java
+++ plugins-src/xmldataexchange/org/bh/plugin/xmldataexchange/xmlimport/DataTypeConverter.java
@@ -1,6 +1,7 @@
 package org.bh.plugin.xmldataexchange.xmlimport;
 
 import java.security.KeyPair;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +12,21 @@ import org.bh.data.types.IntegerValue;
 import org.bh.data.types.IntervalValue;
 import org.bh.data.types.ObjectValue;
 import org.bh.data.types.StringValue;
-import org.jdom.Element;
-import org.jdom.Namespace;
+import nu.xom.*;
+
+/**
+ * 
+ *
+ * @author Vito
+ * @version 1.0, 12.01.2011
+ * @update Vito Masiello, Patrick Maisel
+ * Klasse wurde an die XOM Library angepasst 
+ *
+ */
 
 public class DataTypeConverter {	
 	
-	public static Object[] getIValueRepresenation(Element node, Namespace ns)
+	public static Object[] getIValueRepresenation(Element node, String ns)
 	{
 		Object[] result = new Object[2];
 		String key = node.getAttributeValue("key");
@@ -25,31 +35,33 @@ public class DataTypeConverter {
 		IValue val = null; 
 		if (type.equals("IntegerValue"))
 		{
-			String strValue = ((Element)node.getChildren().get(0)).getValue();
+			String strValue = ((Element)node.getChildElements().get(0)).getValue();
 			val = new IntegerValue(Integer.parseInt(strValue));
 		}
 		else if (type.equals("DoubleValue"))
 		{
-			String strValue = ((Element)node.getChildren().get(0)).getValue();
+			String strValue = ((Element)node.getChildElements().get(0)).getValue();
 			val = new DoubleValue(Double.parseDouble(strValue));
 		}
 		else if (type.equals("StringValue"))
 		{
-			String strValue = ((Element)node.getChildren().get(0)).getValue();
+			String strValue = ((Element)node.getChildElements().get(0)).getValue();
 			val = new StringValue(strValue);
 		}
 		else if (type.equals("IntervalValue"))
 		{
-			Element child = node.getChild("intervalValue", ns);
+			Element child = node.getFirstChildElement("intervalValue", ns);
 			String minValue = child.getAttributeValue("minValue");
 			String maxValue = child.getAttributeValue("maxValue");
 			val = new IntervalValue(Double.parseDouble(minValue), Double.parseDouble(maxValue));
 		}
 		else if (type.equals("ObjectValue"))
 		{
-			Element objectValueChild = node.getChild("objectValue", ns);
+			Element objectValueChild = node.getFirstChildElement("objectValue", ns);
+			ArrayList<Element> listElement = new ArrayList<Element>();
+			listElement.add(objectValueChild);
 			List<DTOKeyPair> keyPairList = new ArrayList<DTOKeyPair>();
-			for (Object child : objectValueChild.getChildren())
+			for (Object child : listElement)
 			{
 				String pairId = ((Element)child).getAttributeValue("id");
 				String pairKey = ((Element)child).getAttributeValue("key");
