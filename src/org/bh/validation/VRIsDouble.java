6558d0ab341e26de6c72538dec6680b9d2a71314
Weber.Simon <Weber.Simon@da734bc7-9a39-418a-a4cd-aaf827679e08>
2011-12-21 10:19:23 +0000

    Valodation rule for double values woth thousands separators

--- src/org/bh/validation/VRIsDouble.java
+++ src/org/bh/validation/VRIsDouble.java
@@ -45,12 +45,12 @@ public class VRIsDouble extends ValidationRule {
 		if (comp instanceof JTextField || comp instanceof BHTextField) {
 			BHTextField tf_toValidate = (BHTextField) comp;
 			double value = Services.stringToDouble(tf_toValidate.getText());
-			if (tf_toValidate.getText().contains(".") && BHTranslator.getLoc() == Locale.GERMAN)
+			if (tf_toValidate.getText().contains(".") && BHTranslator.getLoc() == Locale.GERMAN && ! tf_toValidate.getText().matches("(([1-9]\\d{0,2})(.\\d{3})*)"))
 				validationResult.addError(translator.translate("Efield") + " '"
 						+ translator.translate(tf_toValidate.getKey()) + "' "
 						+ translator.translate("EisDouble"));
 			
-			else if (tf_toValidate.getText().contains(",") && BHTranslator.getLoc()  == Locale.ENGLISH)
+			else if (tf_toValidate.getText().contains(",") && BHTranslator.getLoc()  == Locale.ENGLISH && ! tf_toValidate.getText().matches("(([1-9]\\d{0,2})(,\\d{3})*)"))
 			validationResult.addError(translator.translate("Efield") + " '"
 					+ translator.translate(tf_toValidate.getKey()) + "' "
 					+ translator.translate("EisDouble"));
