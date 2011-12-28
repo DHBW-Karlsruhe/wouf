8dc691efe3c16c1d80ad1cc4d63f509b26ab8de9
Lochner.Lukas <Lochner.Lukas@da734bc7-9a39-418a-a4cd-aaf827679e08>
2011-12-28 10:03:54 +0000

    This is a Test File, that shows how Data can be imported and used from the given Test-XML!

---
+++ tests/src/org/bh/tests/junit/branchSpecificRepresentative/XMLImportTest.java
@@ -0,0 +1,93 @@
+package org.bh.tests.junit.branchSpecificRepresentative;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bh.data.DTOBusinessData;
+import org.bh.data.DTOPeriod;
+import org.bh.plugin.branchSpecificRepresentative.data.DTOBranch;
+import org.bh.plugin.branchSpecificRepresentative.data.DTOCompany;
+import org.bh.plugin.branchSpecificRepresentative.excelImport.ExcelImport;
+import org.bh.plugin.xmldataexchange.xmlimport.XMLImport;
+import org.bh.plugin.xmldataexchange.xmlimport.XMLNotValidException;
+
+import junit.framework.TestCase;
+
+
+/**
+ * This is just a test!
+ * DO NOT USE! THIS IS NOT A UNIT TEST!
+ *
+ * @author Lukas Lochner
+ * @version 1.0, 02.12.2011
+ *
+ */
+
+public class XMLImportTest extends TestCase {
+	
+	ExcelImport myImportClass;	
+	
+	public void testImport()
+	{		
+		
+		XMLImport myImport = new XMLImport("src/org/bh/companydata/periods.xml");
+		try {
+			DTOBusinessData myDTO = (DTOBusinessData) myImport.startImport();
+			
+			//
+			// just play with the BusinessData DTO! :-)
+			// Like...
+			//
+			
+			List<DTOBranch> branchList = myDTO.getChildren();
+			
+			// Iterate all Company DTOs
+			Iterator<DTOBranch> itr = branchList.iterator(); 
+			while(itr.hasNext()) {
+	    		DTOBranch currBranch = itr.next();			
+	    			
+	    		// echos the branch keys
+	    		System.out.println(currBranch.get(DTOBranch.Key.BRANCH_KEY));
+	    		
+	    		//
+	    		// Do the Branch has Companies?	    		
+	    		List<DTOCompany> companyList = currBranch.getChildren();	    		
+	    		Iterator<DTOCompany> CompanyItr = companyList.iterator(); 
+	    		while(CompanyItr.hasNext()) {
+	        		DTOCompany currCompany = CompanyItr.next();			
+	        		
+	        		// echo them
+	        		System.out.println("----" + currCompany.get(DTOCompany.Key.NAME));
+	        		
+	        		//
+	        		// Do the Company has any Periods?
+		    		List<DTOPeriod> periodList = currCompany.getChildren();	    		
+		    		Iterator<DTOPeriod> PeriodItr = periodList.iterator(); 
+		    		while(PeriodItr.hasNext()) {
+		        		DTOPeriod currPeriod = PeriodItr.next();			
+		        		
+		        		// echo the name of the Period
+		        		System.out.println("---------" + currPeriod.get(DTOPeriod.Key.NAME));		        			        		
+		        		
+		    		} 	 	        		
+	        		
+	        		
+	    		} 	    		
+	    		
+	    		
+			} 
+									
+			
+		} catch (XMLNotValidException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+		
+}
