d2e3255d653de38b4f97fb6cc7e995c65661aa0a
Hesse.Guenter <Hesse.Guenter@da734bc7-9a39-418a-a4cd-aaf827679e08>
2012-01-04 10:37:52 +0000

    started with ticket for calculation of BSR rating (ticket assigned on my name)

--- plugins-src/branchSpecificRepresentative/org/bh/plugin/branchSpecificRepresentative/calc/BranchSpecificCalculator.java
+++ plugins-src/branchSpecificRepresentative/org/bh/plugin/branchSpecificRepresentative/calc/BranchSpecificCalculator.java
@@ -10,6 +10,7 @@ import org.bh.data.DTOBranchSpecificRep;
 import org.bh.data.DTOBusinessData;
 import org.bh.data.DTOCompany;
 import org.bh.data.DTOPeriod;
+import org.bh.data.DTOScenario;
 import org.bh.data.types.DoubleValue;
 
 /**
@@ -19,43 +20,43 @@ import org.bh.data.types.DoubleValue;
  * <detailed_description>
  * 
  * @author Denis Roster, Tim Herzenstiel, Sebastian Schumacher
+ * @update Günter Hesse
  * @version 1.0, 21.12.2011
  * 
  */
 public class BranchSpecificCalculator implements IBranchSpecificCalculator {
 
-	public ArrayList<DTOBranchSpecificRep> calculateBSR(DTOBusinessData businessData) {
-		List<DTOBranch> branchList = businessData.getChildren();
+	private static double ratingBSR;
+	private static DTOBranch selectedBranch = null;
 
-		// Iterate all Company DTOs
-		Iterator<DTOBranch> itr = branchList.iterator();
-		while (itr.hasNext()) {
-			DTOBranch currBranch = itr.next();
+	public ArrayList<DTOBranchSpecificRep> calculateBSR(
+			DTOBusinessData businessData) {
+		DTOBranch currBranch = getSelectedBranch(businessData);
 
-			// echos the branch keys
-			System.out.println(currBranch.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY));
+		// echos the branch keys
+		System.out.println(currBranch
+				.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY));
 
-			//
-			// Do the Branch has Companies?
-			List<DTOCompany> companyList = currBranch.getChildren();
-			Iterator<DTOCompany> CompanyItr = companyList.iterator();
-			while (CompanyItr.hasNext()) {
-				DTOCompany currCompany = CompanyItr.next();
+		// Do the Branch has Companies?
+		List<DTOCompany> companyList = currBranch.getChildren();
+		Iterator<DTOCompany> CompanyItr = companyList.iterator();
+		while (CompanyItr.hasNext()) {
+			DTOCompany currCompany = CompanyItr.next();
 
-				// echo them
-				System.out.println("----"
-						+ currCompany.get(DTOCompany.Key.NAME));
+			// echo them
+			System.out.println("----" + currCompany.get(DTOCompany.Key.NAME));
 
-				// Norm all FCF-Period-Values
-				getNormedCFValue("", currCompany);
+			// Norm all FCF-Period-Values
+			getNormedCFValue("", currCompany);
 
-			}
 		}
 
 		// mit normierter BusinessData die Mittelwerte berechnen
 		ArrayList<DTOBranchSpecificRep> dtoBSRaverage = new ArrayList<DTOBranchSpecificRep>();
-		
+
 		dtoBSRaverage = getArithmeticAverage("", businessData);
+
+		computeRating(dtoBSRaverage, businessData);
 
 		return dtoBSRaverage;
 
@@ -101,92 +102,155 @@ public class BranchSpecificCalculator implements IBranchSpecificCalculator {
 
 		ArrayList<DTOBranchSpecificRep> arithmeticAverage = new ArrayList<DTOBranchSpecificRep>();
 
-		List<DTOBranch> branchList = businessDataNormed.getChildren();
 		double avgFCF[] = null;
 
 		// Iterate all Company DTOs
-		Iterator<DTOBranch> itr = branchList.iterator();
-		while (itr.hasNext()) {
-			DTOBranch currBranch = itr.next();
+		DTOBranch currBranch = getSelectedBranch(businessDataNormed);
 
-			// echos the branch keys
-			System.out.println(currBranch.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY));
+		// echos the branch keys
+		System.out.println(currBranch
+				.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY));
+
+		//
+		// Do the Branch has Companies?
+		List<DTOCompany> companyList = currBranch.getChildren();
+		Iterator<DTOCompany> CompanyItr = companyList.iterator();
+		int counter = 0;
+		while (CompanyItr.hasNext()) {
+			DTOCompany currCompany = CompanyItr.next();
+			counter++;
+
+			// echo them
+			System.out.println("----" + currCompany.get(DTOCompany.Key.NAME));
 
 			//
-			// Do the Branch has Companies?
-			List<DTOCompany> companyList = currBranch.getChildren();
-			Iterator<DTOCompany> CompanyItr = companyList.iterator();
-			int counter = 0;
-			while (CompanyItr.hasNext()) {
-				DTOCompany currCompany = CompanyItr.next();
-				counter++;
+			// Do the Company has any Periods?
+			List<DTOPeriod> periodList = currCompany.getChildren();
+			Iterator<DTOPeriod> PeriodItr = periodList.iterator();
+			double sumFCF = 0;
+			DoubleValue helper = null;
+			int innerCounter = 0;
+			while (PeriodItr.hasNext()) {
+				DTOPeriod currPeriod = PeriodItr.next();
 
-				// echo them
-				System.out.println("----"
-						+ currCompany.get(DTOCompany.Key.NAME));
-
-				//
-				// Do the Company has any Periods?
-				List<DTOPeriod> periodList = currCompany.getChildren();
-				Iterator<DTOPeriod> PeriodItr = periodList.iterator();
-				double sumFCF = 0;
-				DoubleValue helper = null;
-				int innerCounter = 0;
-				while (PeriodItr.hasNext()) {
-					DTOPeriod currPeriod = PeriodItr.next();
-
-					// echo the name of the Period
-					System.out.println("---------"
-							+ currPeriod.get(DTOPeriod.Key.NAME));
-					helper = (DoubleValue) currPeriod.get(DTOPeriod.Key.FCF);
-					sumFCF = sumFCF + helper.getValue();
-					innerCounter++;
-				}
-				avgFCF[counter] = sumFCF / innerCounter;
-
+				// echo the name of the Period
+				System.out.println("---------"
+						+ currPeriod.get(DTOPeriod.Key.NAME));
+				helper = (DoubleValue) currPeriod.get(DTOPeriod.Key.FCF);
+				sumFCF = sumFCF + helper.getValue();
+				innerCounter++;
 			}
-
-			double avgBranchFCF = 0;
-
-			// Durchschnittlicher FCF für eine Branche berechnen
-
-			if (choice == "") {
-				for (int i = 0; i <= avgFCF.length; i++) {
-					avgBranchFCF = avgBranchFCF + avgFCF[i];
-				}
-				avgBranchFCF = avgBranchFCF / avgFCF.length;
-				
-			} else {
-				// else-Routine --> gestutzter Mittelwert
-				
-				// neues Array, da kleinster und größter Wert aus avgFCF "gelöscht" werden
-				double[] avgFCFcuted = new double[avgFCF.length-2];
-								
-				// avgFCF-Array aufsteigend sortieren
-				java.util.Arrays.sort(avgFCF);
-				
-				// kleinster und größter Wert weglassen und neues Array befüllen
-				for(int i = 0; i < avgFCF.length-2;i++){
-					avgFCFcuted[i] = avgFCF[i+1];
-					avgBranchFCF = avgBranchFCF + avgFCFcuted[i];
-				}
-				
-				avgBranchFCF = avgBranchFCF / avgFCFcuted.length;
-
-			}
-
-			arithmeticAverage.add(new DTOBranchSpecificRep(""
-					+ currBranch.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY), avgBranchFCF));
+			avgFCF[counter] = sumFCF / innerCounter;
 
 		}
+
+		double avgBranchFCF = 0;
+
+		// Durchschnittlicher FCF für eine Branche berechnen
+
+		if (choice == "") {
+			for (int i = 0; i <= avgFCF.length; i++) {
+				avgBranchFCF = avgBranchFCF + avgFCF[i];
+			}
+			avgBranchFCF = avgBranchFCF / avgFCF.length;
+
+		} else {
+			// else-Routine --> gestutzter Mittelwert
+
+			// neues Array, da kleinster und größter Wert aus avgFCF
+			// "gelöscht" werden
+			double[] avgFCFcuted = new double[avgFCF.length - 2];
+
+			// avgFCF-Array aufsteigend sortieren
+			java.util.Arrays.sort(avgFCF);
+
+			// kleinster und größter Wert weglassen und neues Array befüllen
+			for (int i = 0; i < avgFCF.length - 2; i++) {
+				avgFCFcuted[i] = avgFCF[i + 1];
+				avgBranchFCF = avgBranchFCF + avgFCFcuted[i];
+			}
+
+			avgBranchFCF = avgBranchFCF / avgFCFcuted.length;
+
+		}
+
+		arithmeticAverage.add(new DTOBranchSpecificRep(""
+				+ currBranch.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY),
+				avgBranchFCF));
 
 		return arithmeticAverage;
 
 	}
 
-	public double getRating(DTOBranch[] branchRating) {
-		throw new UnsupportedOperationException(
-				"This method has not been implemented");
+	private void computeRating(ArrayList<DTOBranchSpecificRep> branchSpecificRepresentative,DTOBusinessData businessData){
+			
+		DTOBranch currBranch = getSelectedBranch(businessData);
+
+		// echos the branch keys
+		System.out.println(currBranch
+				.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY));
+
+		// Do the Branch has Companies?
+		List<DTOCompany> companyList = currBranch.getChildren();
+		Iterator<DTOCompany> CompanyItr = companyList.iterator();
+		
+		double result = 0;
+		
+		while (CompanyItr.hasNext()) {
+			DTOCompany currCompany = CompanyItr.next();
+
+			// echo them
+			System.out.println("----" + currCompany.get(DTOCompany.Key.NAME));
+
+			// Norm all FCF-Period-Values
+			getNormedCFValue("", currCompany);
+			
+			List<DTOPeriod> periodList = currCompany.getChildren();
+			Iterator<DTOPeriod> periodItr = periodList.iterator();
+			
+			// iteriert über die einzelnen Perioden der Unternehmen der selektierten Branche
+			// zur Güteberechnung des vorher berechneten, branchenspezifischen Vertreters
+			for (int count = 0; periodItr.hasNext(); count++){
+				DTOPeriod currPeriod = periodItr.next();
+				
+				DoubleValue normedCFforPeriod = (DoubleValue) currPeriod.get(DTOPeriod.Key.FCF);
+				
+				//result = result + ( (normedCFforPeriod - [count]) * (normedCFforPeriod - BSR[count]) ); 
+				
+			}
+
+		}
+		result = result / (companyList.size());
+}
+
+	private DTOBranch getSelectedBranch(DTOBusinessData businessData) {
+		if (this.selectedBranch != null)
+			return this.selectedBranch;
+
+		List<DTOBranch> branchList = businessData.getChildren();
+
+		String selectedBranch = DTOScenario.Key.INDUSTRY.toString();
+
+		// Iterate Company DTOs
+		Iterator<DTOBranch> itr = branchList.iterator();
+		while (itr.hasNext()) {
+			DTOBranch currBranch = itr.next();
+			String currKey = ""
+					+ (currBranch.get(DTOBranch.Key.BRANCH_KEY_MAIN_CATEGORY))
+					+ (currBranch.get(DTOBranch.Key.BRANCH_KEY_MID_CATEGORY))
+					+ (currBranch.get(DTOBranch.Key.BRANCH_KEY_SUB_CATEGORY));
+
+			if (currKey == selectedBranch)
+				this.selectedBranch = currBranch;
+			return currBranch;
+		}
+		// selected branch not found
+		// TODO create an exception
+		return null;
+	}
+
+	public double getRating() {
+		return BranchSpecificCalculator.ratingBSR;
 	}
 
 }
