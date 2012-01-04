4dab8b76016883bc5905228d1165ace4fa19a189
Lochner.Lukas <Lochner.Lukas@da734bc7-9a39-418a-a4cd-aaf827679e08>
2012-01-04 12:55:08 +0000

    New enhancements for Balance-Sheet and ProfitStatements

--- tests/src/org/bh/tests/junit/branchSpecificRepresentative/XMLImportTest.java
+++ tests/src/org/bh/tests/junit/branchSpecificRepresentative/XMLImportTest.java
@@ -8,6 +8,12 @@ import org.bh.data.DTOBranch;
 import org.bh.data.DTOBusinessData;
 import org.bh.data.DTOCompany;
 import org.bh.data.DTOPeriod;
+import org.bh.data.IPeriodicalValuesDTO;
+import org.bh.data.types.Calculable;
+import org.bh.platform.Services;
+import org.bh.plugin.gcc.data.DTOGCCBalanceSheet;
+import org.bh.plugin.gcc.data.DTOGCCProfitLossStatementCostOfSales;
+import org.bh.plugin.gcc.data.DTOGCCProfitLossStatementTotalCost;
 import org.bh.plugin.xmldataexchange.xmlimport.XMLImport;
 import org.bh.plugin.xmldataexchange.xmlimport.XMLNotValidException;
 
@@ -28,6 +34,9 @@ public class XMLImportTest extends TestCase {
 	public void testImport()
 	{		
 		
+		// Init Number Serices
+		Services.initNumberFormats();		
+		
 		XMLImport myImport = new XMLImport("src/org/bh/companydata/periods.xml");
 		try {
 			DTOBusinessData myDTO = (DTOBusinessData) myImport.startImport();
@@ -36,6 +45,8 @@ public class XMLImportTest extends TestCase {
 			// just play with the BusinessData DTO! :-)
 			// Like...
 			//
+			
+			int counter = 0;
 			
 			List<DTOBranch> branchList = myDTO.getChildren();
 			
@@ -67,7 +78,37 @@ public class XMLImportTest extends TestCase {
 		        		DTOPeriod currPeriod = PeriodItr.next();			
 		        		
 		        		// echo the name of the Period
-		        		System.out.println("---------" + currPeriod.get(DTOPeriod.Key.NAME));		        			        		
+		        		System.out.println("---------" + currPeriod.get(DTOPeriod.Key.NAME));		    
+		        		counter++;
+		        		
+		        		//
+		        		//	NEW ENHANCEMENT 04.01.2012
+		        		//
+		        		
+		        		// get the Balance Sheet
+		        		DTOGCCBalanceSheet myCurrBalanceSheet = (DTOGCCBalanceSheet) currPeriod.getChild(0);		
+		        		
+		        				        				     		        	
+		        		//
+		        		// TotalCosts or CostOfSales Method?
+		        		
+		        		// get the ProfitLossStatements
+		        		IPeriodicalValuesDTO myProfitStatement = currPeriod.getChild(1);		        		
+		        		// TotalCost Method
+		        		if(myProfitStatement.getUniqueId() == DTOGCCProfitLossStatementTotalCost.getUniqueIdStatic()) {
+									        		
+		        			DTOGCCProfitLossStatementTotalCost myProfitStatementCasted = (DTOGCCProfitLossStatementTotalCost) myProfitStatement;		        			
+		        			
+		        		}
+		        		
+		        		// CostOfSales Method
+		        		if(myProfitStatement.getUniqueId() == DTOGCCProfitLossStatementCostOfSales.getUniqueIdStatic()) {									        			        
+			        		
+		        			DTOGCCProfitLossStatementCostOfSales myProfitStatementCasted = (DTOGCCProfitLossStatementCostOfSales) myProfitStatement;
+		        			
+		        		}
+		        		
+		        				        		       		        
 		        		
 		    		} 	 	        		
 	        		
@@ -76,6 +117,8 @@ public class XMLImportTest extends TestCase {
 	    		
 	    		
 			} 
+			
+			System.out.println(counter);
 									
 			
 		} catch (XMLNotValidException e) {
