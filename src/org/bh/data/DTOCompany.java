493370fc541d1feebae218ceb92862e1503b23c9
Roedl.Yannick <Roedl.Yannick@da734bc7-9a39-418a-a4cd-aaf827679e08>
2012-01-09 18:19:01 +0000

    Logischer Fehler in der Reihenfolge
    refs #285

--- src/org/bh/data/DTOCompany.java
+++ src/org/bh/data/DTOCompany.java
@@ -61,8 +61,8 @@ public class DTOCompany extends  DTO<DTOPeriod> {
 		DTOPeriod current = null;
 		for(DTOPeriod period : periods){
 			if(current != null){
-				period.previous = current;
-				current.next = period;
+				current.previous = period;
+				period.next = current;
 			}
 			
 			period.scenario = scenario;
