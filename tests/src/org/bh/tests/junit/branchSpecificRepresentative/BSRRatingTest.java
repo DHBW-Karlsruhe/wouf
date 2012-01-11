20f5cdd0a2776b6b5154aea7acbddf56b4615e21
Hesse.Guenter <Hesse.Guenter@da734bc7-9a39-418a-a4cd-aaf827679e08>
2012-01-11 12:23:36 +0000

    JUnit Test zu fix #233 - Güteberechnung

---
+++ tests/src/org/bh/tests/junit/branchSpecificRepresentative/BSRRatingTest.java
@@ -0,0 +1,73 @@
+package org.bh.tests.junit.branchSpecificRepresentative;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.bh.data.DTO;
+import org.bh.data.DTOBusinessData;
+import org.bh.data.DTOCompany;
+import org.bh.platform.Services;
+import org.bh.plugin.branchSpecificRepresentative.calc.BranchSpecificCalculator;
+import org.bh.plugin.xmldataexchange.xmlimport.XMLImport;
+import org.bh.plugin.xmldataexchange.xmlimport.XMLNotValidException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+
+/**
+ * <short_description>
+ * 
+ * <p>
+ * <detailed_description>
+ * 
+ * @author Günter Hesse
+ * @version 1.0, 11-Jan-2012
+ * 
+ */
+public class BSRRatingTest extends TestCase {
+
+	@Before
+	public void setUp() {
+		DTO.setThrowEvents(false); // Damit die Logfiles nicht so groß werden.
+	}
+
+	@After
+	public void tearDown() {
+		DTO.setThrowEvents(true);
+	}
+
+	public void testBranchCalculator() {
+
+		// Init Number Serices
+		Services.initNumberFormats();
+
+		XMLImport myImport = new XMLImport("src/org/bh/companydata/periods.xml");
+
+		// BusinessData aufbauen
+		DTOBusinessData myDTO = null;
+		
+		try {
+			myDTO = (DTOBusinessData) myImport.startImport();
+		} catch (XMLNotValidException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Default-Konstruktor aufrufen
+		BranchSpecificCalculator bsc = new BranchSpecificCalculator();
+
+		// Branch-Specific-Representative ermitteln lassen (Normierung &
+		// Mittelwert)
+		DTOCompany bsr = new DTOCompany();
+
+		// Branchspezifischen Vertreter ermitteln (= normieren der CFs &
+		// Mittelwertsberechnung (normal/gestutzt))
+		bsr = bsc.calculateBSR(myDTO, null);
+
+		Assert.assertTrue(bsc.getRating() == 316637.4899836928);
+
+	}
+
+}
